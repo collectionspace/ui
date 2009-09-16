17208b0c1df756f10642b4efbbd519ec74b20cc9
Jesse Martinez <mjesse@gmail.com>
2009-09-16 21:20:48 +0000

    CSPACE-456: Created acquisition.html page based on wireframe: http://wiki.collectionspace.org/download/attachments/14647859/Acquisition.png
    Additionally, made small improvements to usability of collapsible headers; size, arrangement of specific fields; misc. edits to CSS to improve design.

--- src/main/webapp/js/cspace.js
+++ src/main/webapp/js/cspace.js
@@ -4,3 +4,12 @@ function ShowHide(toggleme) {
 	var src = ($(self).attr("src") === "../images/toggle-more.png") ? "../images/toggle-less.png" : "../images/toggle-more.png";
 	$(self).attr("src", src);
 }
+
+function getRadioValue() {
+	for (var i=0; i < document.selectprocedure.procedure.length; i++) {
+		if (document.selectprocedure.procedure[i].checked) {
+			var rad_val = document.selectprocedure.procedure[i].value;
+			window.open(rad_val, "_self", "");
+		}
+	}
+}
