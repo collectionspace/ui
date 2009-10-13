60876a7e941ae3bb38da9b36e6811c64d74c5c40
Anastasia Cheetham <acheetham@ocad.ca>
2009-10-13 18:40:42 +0000

    CSPACE-460: Committing CarlH's work on the acquisition page: Apply the DataEntry component to the HTML, including schema and test data; Modified the Create New page to enable the Acquisition record radio button.

---
+++ src/main/webapp/js/acquisitionSetup.js
@@ -0,0 +1,41 @@
+/*
+Copyright 2009 University of Toronto
+Copyright 2009 University of Cambridge
+
+Licensed under the Educational Community License (ECL), Version 2.0. 
+You may not use this file except in compliance with this License.
+
+You may obtain a copy of the ECL 2.0 License at
+https://source.collectionspace.org/collection-space/LICENSE.txt
+*/
+
+/*global jQuery, window, cspace*/
+
+var demo = demo || {};
+
+(function ($) {
+
+    demo.setup = function () {
+        var csid = cspace.util.getUrlParameter("csid");
+        var oiOpts = {
+            autoGenNumField: "acquReferenceNum",
+            alternateFields: ["acquReferenceNum"],
+            uiSpecUrl: "./schemas/acquisition/schema.json",
+            templates: {
+                body: {
+                    url: "../html/acquisitionTemplate.html",
+                    id: "csc-acquisition-template"
+                }
+            }
+        };
+        if (csid) {
+            oiOpts.csid = csid;
+        }
+        if (document.location.protocol === "file:") {
+            oiOpts.dataContext = cspace.util.setupTestDataContext("acquisition");
+        }
+        var acquisition = cspace.dataEntry(".csc-acquisition-container", oiOpts);
+    };
+    
+})(jQuery);
+
