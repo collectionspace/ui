74c75d6c51bef61c9a9e9b7183250155ec538773
Anastasia Cheetham <acheetham@ocad.ca>
2009-11-10 16:31:03 +0000

    CSPACE-507: Update the unit tests to actually run.

--- src/test/js/NumberPatternChooserTest.js
+++ src/test/js/NumberPatternChooserTest.js
@@ -1,13 +1,43 @@
+/*
+Copyright 2009 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0. 
+ou may not use this file except in compliance with this License.
+
+You may obtain a copy of the ECL 2.0 License at
+https://source.collectionspace.org/collection-space/LICENSE.txt
+*/
+
 /*global jQuery, jqUnit, cspace*/
+
 (function ($) {
     
     $(document).ready(function () {
+        var chooser;
+        var model = {
+		    "list": ["val1", "val2", "val3"],
+			"samples": ["sample1", "sample2", "sample3"],
+			"names": ["name1", "name2", "name3"]
+        };
+        var options = {
+                listeners: {
+                    afterRender: function () {
+                        start();
+                    }
+                },
+                model: model,
+                templateUrl: "../../main/webapp/html/NumberPatternChooser.html"
+            };
 
-        var numberPatternChooserTest = new jqUnit.TestCase("NumberPatternChooser Tests");
+        var mySetup = function () {
+            stop();
+            chooser = cspace.numberPatternChooser(".info-value", options);
+        };
 
-//	    numberPatternChooserTest.test("Construction", function () {
-            var chooser = cspace.numberPatternChooser(".info-value",
-                                    {templateURL: "../../main/webapp/html/NumberPatternChooser.html"});
-//	    });
+        var numberPatternChooserTest = new jqUnit.TestCase("NumberPatternChooser Tests", mySetup);
+
+	    numberPatternChooserTest.test("Construction", function () {
+            jqUnit.isVisible("Number pattern chooser button should be visible", $(".csc-numberPatternChooser-button"));
+	    });
     });
 })(jQuery);
