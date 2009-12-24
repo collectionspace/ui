657d288fcf94f9f823858ee00bb14f3e3b1ec58b
Jesse Martinez <mjesse@gmail.com>
2009-12-24 19:28:01 +0000

    CSPACE-625: created authority.html page. This has a toggling tab menu to switch between Person Name Authority fields and Person Contact Information fields. Organization fields need to be added.

--- src/main/webapp/js/cspace.js
+++ src/main/webapp/js/cspace.js
@@ -4,14 +4,23 @@ function ShowHide(toggleme) {
 	var src = (jQuery(self).attr("src") === "../images/toggle-more.png") ? "../images/toggle-less.png" : "../images/toggle-more.png";
 	jQuery(self).attr("src", src);
 }
+
+function ToggleTabs(toggleme, toggleother, toggleSelected) {
+	jQuery(toggleother).hide();
+	jQuery(toggleme).show();
+	jQuery(toggleSelected).removeClass("selected");
+	
+}
  
 function getRadioValue() {
-	for (var i=0; i < document.selectprocedure.procedure.length; i++) {
-		if (document.selectprocedure.procedure[i].checked) {
-			var rad_val = document.selectprocedure.procedure[i].value;
+	for (var i=0; i < document.selecttype.recordtype.length; i++) {
+		if (document.selecttype.recordtype[i].checked) {
+			var rad_val = document.selecttype.recordtype[i].value;
 			window.open(rad_val, "_self", "");
+			return;
 		}
 	}
+
 }
 
 
