aa3e7031a98afe6f13a66c12158982890ec2e0b1
Anastasia Cheetham <acheetham@ocad.ca>
2010-01-29 22:46:52 +0000

    CSPACE-825: Start at a PageBuilder. The tests are just a sketch/maquette, used to flesh out requirements. Currently not quite functional, but I don't want to go home for the weekend leaving this just sitting on my computer.

---
+++ src/main/webapp/js/PageBuilder.js
@@ -0,0 +1,58 @@
+/*
+Copyright 2010 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0. 
+ou may not use this file except in compliance with this License.
+
+You may obtain a copy of the ECL 2.0 License at
+https://source.collectionspace.org/collection-space/LICENSE.txt
+*/
+
+/*global jQuery, fluid_1_2*/
+
+var cspace = cspace || {};
+
+(function ($, fluid) {
+
+    // Notes:
+    // the HTML has been constructed on the server
+    // and the UISpec has been provided through JSONP, stored in cspace.pageBuilder.uispec
+
+    var setup = function (that) {
+        that.uispec = cspace.pageBuilder.uispec;
+        that.expander = fluid.renderer.makeProtoExpander({ELstyle: "${}"});
+        for (var region in that.uispec) {
+            if (that.uispec.hasOwnProperty(region)) {
+                var tree = that.expander(that.uispec[region]);
+                
+                // note: renderer options like debugMode should be passed in, not hard-coded
+                var renderOpts = {
+                    debugMode: true
+                };
+                if (that.options.model) {
+                    if (that.options.modelPath) {
+                        renderOpts.model = that.options.model[modelPath];
+                    } else {
+                        renderOpts.model = that.options.model;
+                    }
+                }
+                fluid.selfRender(that.container, tree, renderOpts);
+            }
+        }
+
+        var scriptBlocks = $("script", that.container);
+        scriptBlocks.each(function (index, domElement) {
+            var block = $(domElement);
+            var parent = block.parent();
+            block.remove();
+            parent.append(block);
+        });
+    };
+
+    cspace.pageBuilder = function (container, options) {
+        var that = fluid.initView("cspace.pageBuilder", container, options);
+
+        setup(that);
+    };
+
+})(jQuery, fluid_1_2);
---
+++ src/test/js/PageBuilderTest.js
@@ -0,0 +1,91 @@
+/*
+Copyright 2010 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0. 
+ou may not use this file except in compliance with this License.
+
+You may obtain a copy of the ECL 2.0 License at
+https://source.collectionspace.org/collection-space/LICENSE.txt
+*/
+
+/*global cspace, jqUnit, jQuery, start, stop, expect*/
+
+var pageBuilderTester = function () {
+    /*
+     * Utility to call qUnit's start() only once all tests have executed
+     */
+    var done; 
+    var startIfDone = function (numTests) {
+        if (++done === numTests) {
+            done = 0;
+            start();
+        }
+    };
+
+    var pageBuilderTest = new jqUnit.TestCase("PageBuilder Tests");
+    
+    cspace.testComponent1 = function (container, options) {
+        var jContainer = $(container);
+        jqUnit.assertEquals("Number of element rendered", 2, $(".inside-container", jContainer).length);
+        startIfDone(2);
+    };
+
+    cspace.testComponent2 = function (container, options) {
+        var jContainer = $(container);
+        jqUnit.assertEquals("Number of element rendered", 1, $(".inside-container", jContainer).length);
+        startIfDone(2);
+    };
+
+    pageBuilderTest.test("ProtoComponents", function () {
+        // this doesn't work yet because i'm  not using selectors:
+        // rendering the first proto-tree removes the second because the second
+        // has an rsf:id but no matching node in the tree
+        var testProtoTree1 = {
+            title: "${objectTitle}",
+            description: "${briefDescription}",
+            dataEntryContainer: {
+                decorators: [
+                    {type: "fluid",
+                     func: "cspace.testComponent1",
+                     container: "#dataEntryContainer",
+                     options: {modelPath: "fields"} }
+                ]
+            }
+        };
+        var testProtoTree2 = {
+            link: {target: "${recordType}.html?csid=${csid}",
+                    linktext: "${number}"},
+            linksContainer: {
+                decorators: [
+                    {type: "fluid",
+                     func: "cspace.testComponent2",
+                     container: "#linksContainer",
+                     options: {modelPath: "relations"} }
+                ]
+            }
+        };
+        cspace.pageBuilder.uispec = {
+            dataEntry: testProtoTree1,
+            relatedRecords: testProtoTree2
+        };
+        var testModel = {
+            fields: {
+                objectTitle: "Rendered title",
+                briefDescription: "Rendered description"
+            },
+            relations: {
+                recordType: "object",
+                number: "2005.1",
+                csid: "12345"
+            }
+        };
+        done = 0;
+        stop();
+        var pageBuilder = cspace.pageBuilder("#protoTreeTest", {model: testModel});
+    });
+};
+
+    
+(function () {
+    pageBuilderTester();
+}());
