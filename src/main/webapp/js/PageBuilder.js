401d6db9d16770434821a2420d83e60e299053ec
Anastasia Cheetham <acheetham@ocad.ca>
2010-02-02 22:22:33 +0000

    CSPACE-825: Implementing new dependencies and pageSpec approach. Still not complete, but basics are functional.

--- src/main/webapp/js/PageBuilder.js
+++ src/main/webapp/js/PageBuilder.js
@@ -10,14 +10,74 @@ https://source.collectionspace.org/collection-space/LICENSE.txt
 
 /*global jQuery, fluid_1_2*/
 
-var cspace = cspace || {};
+cspace = cspace || {};
 
 (function ($, fluid) {
 
     // Notes:
-    // the HTML has been constructed on the server
-    // and the UISpec has been provided through JSONP, stored in cspace.pageBuilder.uispec
+    // the UISpec has been provided through JSONP, stored in cspace.pageBuilder.uispec
 
+    var setup = function (that) {
+        that.uispec = cspace.pageBuilder.uispec;
+        that.components = [];
+        for (var region in that.dependencies) {
+			if (that.dependencies.hasOwnProperty(region)) {
+	            var dep = that.dependencies[region];
+	            that.components[region] = fluid.invokeGlobalFunction(dep.funcName, dep.args);
+			}
+        }
+    };
+
+    var assembleHTML = function (that) {
+        fluid.fetchResources(that.options.pageSpec, function (resourceSpecs) {
+            for (var regionName in resourceSpecs) {
+                if (resourceSpecs.hasOwnProperty(regionName) && (regionName !== "callbackCalled")) {
+                    var region = resourceSpecs[regionName];
+                    var doc = $(region.resourceText, document);
+                    if (doc) {
+//                        var node = $(region.templateSelector, doc);
+var node = doc;
+                        if (node) {
+                            $(region.targetSelector).replaceWith(node);
+                        } else {
+//                            console.log("Couldn't find template node in HTML");
+                        }
+                    } else {
+//                        console.log("Couldn't convert HTML to a jQuery");
+                    }
+                }
+                
+            }
+            setup(that);
+        });
+
+    };
+
+    cspace.pageBuilder = function (dependencies, options) {
+        var that = {
+            dependencies: dependencies
+        };
+        if (options && options.container) {
+            // not sure exactly what to do here, or what condition to check to decide to do it
+            that = fluid.initView("cspace.pageBuilder", options.container, options);
+        } else {
+            fluid.mergeComponentOptions(that, "cspace.pageBuilder", options);
+        }
+
+        if (options && options.pageSpec) {
+            assembleHTML(that);
+        } else {
+            setup(that);
+        }
+    };
+
+})(jQuery, fluid_1_2);
+
+
+// the following code snippets will have to be moved somewhere accessible to the components
+// I know commented code is bad, but I don't want to lose this work right now.
+
+/*
     var buildCutpoints = function (uispec) {
         var cutpoints = [];
         for (var key in uispec) {
@@ -31,12 +91,47 @@ var cspace = cspace || {};
         return cutpoints;
     };
 
-    var setup = function (that) {
-        that.uispec = cspace.pageBuilder.uispec;
+    var extractEL = function (string) {
+        var i1 = string.indexOf("${");
+        var i2 = string.indexOf("}");
+        if (i1 === 0 && i2 !== -1) {
+            return string.substring(2, i2);
+        }
+    };
+
+    var makeProtoTree = function (uispec, model) {
+        var protoTree = {};
+
+        // should this be protoTree = fluid.copy(uispec)?
+        fluid.model.copyModel(protoTree, uispec);
+        
+        for (var key in protoTree) {
+            if (protoTree.hasOwnProperty(key)) {
+                if (fluid.isArrayable(protoTree[key]) && protoTree[key][0].repeatFromModel) {
+                    // replace protoTree[key] with a new array
+                    var rows = [];
+                    var elPath = extractEL(protoTree[key][0].repeatFromModel);
+                    elPath = elPath.slice(0, elPath.lastIndexOf(".0"));
+                    var numRows = fluid.model.getBeanValue(model,elPath).length;
+                    for (var i = 0; i < numRows; i++) {
+                        rows.push(protoTree[key][0].repeatFromModel.replace("0", i));
+                    }
+                    protoTree[key] = rows;
+                } else if (key === "buildValue") {
+                    
+                } else {
+                    // leave it, no change necessary
+                }
+            }
+        }
+        return protoTree;
+    };
+
         that.expander = fluid.renderer.makeProtoExpander({ELstyle: "${}"});
         for (var region in that.uispec) {
             if (that.uispec.hasOwnProperty(region)) {
-                var tree = that.expander(that.uispec[region]);
+                var protoTree = makeProtoTree(that.uispec[region], that.options.model);
+                var tree = that.expander(protoTree);
                 
                 // note: renderer options like debugMode should be passed in, not hard-coded
                 var renderOpts = {
@@ -53,20 +148,4 @@ var cspace = cspace || {};
                 fluid.selfRender(that.container, tree, renderOpts);
             }
         }
-
-        var scriptBlocks = $("script", that.container);
-        scriptBlocks.each(function (index, domElement) {
-            var block = $(domElement);
-            var parent = block.parent();
-            block.remove();
-            parent.append(block);
-        });
-    };
-
-    cspace.pageBuilder = function (container, options) {
-        var that = fluid.initView("cspace.pageBuilder", container, options);
-
-        setup(that);
-    };
-
-})(jQuery, fluid_1_2);
+*/
--- src/test/js/PageBuilderTest.js
+++ src/test/js/PageBuilderTest.js
@@ -10,7 +10,9 @@ https://source.collectionspace.org/collection-space/LICENSE.txt
 
 /*global cspace, jqUnit, jQuery, start, stop, expect*/
 
-var pageBuilderTester = function () {
+cspace = cspace || {};
+
+(function ($) {
     /*
      * Utility to call qUnit's start() only once all tests have executed
      */
@@ -22,67 +24,190 @@ var pageBuilderTester = function () {
         }
     };
 
-    var pageBuilderTest = new jqUnit.TestCase("PageBuilder Tests");
+    cspace.testData = cspace.testData || {};
+
+    cspace.testData.basicDependencies = {
+        dateEntry: {
+            funcName: "cspace.testComponent1",
+            args: [
+                "#dataEntryContainer"   // container
+//                ,{modelPath: "fields"}  // options
+            ]
+        },
+        relatedRecords: {
+            funcName: "cspace.testComponent2",
+            args: [
+                "#linksContainer"           // container
+//                ,{modelPath: "relations"}   // options
+            ]
+        }
+    };
+
+    cspace.testData.testPageSpec = {
+        dataEntry: {
+            href: "test-data/template1.html",
+            templateSelector: "#template1mainNode",
+            targetSelector: "#insertTemplate1here"
+        },
+        sidebar: {
+            href: "test-data/template2.html",
+            templateSelector: "#template2mainNode",
+            targetSelector: "#insertTemplate2here"
+        }
+    };
+
+    // these test components carry out tests on the markup, or on their parameters. they are
+    // customized for the test functions that use them
     
     cspace.testComponent1 = function (container, options) {
         var jContainer = $(container);
-        jqUnit.assertEquals("Number of element rendered", 2, $(".inside-container", jContainer).length);
-        startIfDone(2);
+        jqUnit.assertTrue("TestComponent1 instantiated", true);
+        jqUnit.assertEquals("TestComponent1 initiated with correct container", cspace.testData.basicDependencies.dateEntry.args[0], container);
     };
 
     cspace.testComponent2 = function (container, options) {
         var jContainer = $(container);
-        jqUnit.assertEquals("Number of element rendered", 1, $(".inside-container", jContainer).length);
+        jqUnit.assertTrue("TestComponent2 instantiated", true);
+        jqUnit.assertEquals("TestComponent2 initiated with correct container", cspace.testData.basicDependencies.relatedRecords.args[0], container);
+    };
+
+    cspace.testComponent3 = function (container, options) {
+        var jContainer = $(container);
+        jqUnit.assertEquals("Template 1 inserted", 1, $("#testText1").length);
         startIfDone(2);
     };
 
-    pageBuilderTest.test("ProtoComponents", function () {
-        var testProtoTree1 = {
-            title: "${fields.objectTitle}",
-            description: "${fields.briefDescription}",
-            dataEntryContainer: {
-                decorators: [
-                    {type: "fluid",
-                     func: "cspace.testComponent1",
-                     container: "#dataEntryContainer",
-                     options: {modelPath: "fields"} }
-                ]
-            }
-        };
-        var testProtoTree2 = {
-            link: {target: "${relations.recordType}.html?csid=${csid}",
-                    linktext: "${relations.number}"},
-            linksContainer: {
-                decorators: [
-                    {type: "fluid",
-                     func: "cspace.testComponent2",
-                     container: "#linksContainer",
-                     options: {modelPath: "relations"} }
-                ]
-            }
-        };
-        cspace.pageBuilder.uispec = {
-            dataEntry: testProtoTree1,
-            relatedRecords: testProtoTree2
-        };
-        var testModel = {
-            fields: {
-                objectTitle: "Rendered title",
-                briefDescription: "Rendered description"
-            },
-            relations: {
-                recordType: "object",
-                number: "2005.1",
-                csid: "12345"
-            }
-        };
-        done = 0;
-        stop();
-        var pageBuilder = cspace.pageBuilder("#protoTreeTest", {model: testModel});
-    });
-};
+    cspace.testComponent4 = function (container, options) {
+        var jContainer = $(container);
+        jqUnit.assertEquals("Template 2 inserted", 1, $("#testText2").length);
+        startIfDone(2);
+    };
 
+    var pageBuilderTester = function () {
+        var pageBuilderTest = new jqUnit.TestCase("PageBuilder Tests");
+        
+        pageBuilderTest.test("Invocation of dependent components", function () {
+            expect(4);    // this is total num of assertions in the test components
+            cspace.pageBuilder(cspace.testData.basicDependencies);
+        });
     
-(function () {
-    pageBuilderTester();
-}());
+        pageBuilderTest.test("Assembly of HTML", function () {
+            expect(2);
+            var dependencies = {
+                dateEntry: {
+                    funcName: "cspace.testComponent3",
+                    args: [
+                        "#dataEntryContainer"   // container
+                    ]
+                },
+                relatedRecords: {
+                    funcName: "cspace.testComponent4",
+                    args: [
+                        "#linksContainer"           // container
+                    ]
+                }
+            }
+            var options = {
+                pageSpec: cspace.testData.testPageSpec
+            };
+            done = 0;
+            stop();
+            cspace.pageBuilder(dependencies, options);
+        });
+    };
+
+    $(document).ready(function () {
+        pageBuilderTester();
+    });
+
+})(jQuery);
+    
+// The following code bits will be useful in future tests, but not right now,
+// so it is temporarily commented out. I want the currently working tests to be
+// as clear as possible
+// I know commented code is bad, but I don't want to lose this work right now.
+
+/*
+    cspace.testData.testUISpec1 = {
+        title: "${fields.objectTitle}",
+        description: "${fields.briefDescription}",
+        method: [
+//need "children" here!!!
+            {repeatFromModel: "${fields.entryMethod.0}"}
+        ]
+    };
+    cspace.testData.testUISpec2 = {
+        link: {
+            target: {buildValue: "${relations.recordType}.html?csid=${csid}"},
+            linktext: "${relations.number}"
+        }
+    };
+    cspace.pageBuilder.uispec = {
+        dateEntry: cspace.testData.testUISpec1,
+        relatedRecords: cspace.testData.testUISpec2
+    };
+    cspace.testData.testModel = {
+        fields: {
+            objectTitle: "Rendered title",
+            briefDescription: "Rendered description",
+            entryMethod: ["foo", "bar", "bat"]
+        },
+        relations: {
+            recordType: "object",
+            number: "2005.1",
+            csid: "12345"
+        }
+    };
+*/
+
+    //    pageBuilderTest.test("ProtoComponents", function () {
+    //        var testProtoTree1 = {
+    //            title: "${fields.objectTitle}",
+    //            description: "${fields.briefDescription}",
+    //            method: [
+    ////need "children" here!!!
+    //                {repeatFromModel: "${fields.entryMethod.0}"}
+    //            ],
+    //            dataEntryContainer: {
+    //                decorators: [
+    //                    {type: "fluid",
+    //                     func: "cspace.testComponent1",
+    //                     container: "#dataEntryContainer",
+    //                     options: {modelPath: "fields"} }
+    //                ]
+    //            }
+    //        };
+    //        var testProtoTree2 = {
+    //            link: {
+    //                target: {buildValue: "${relations.recordType}.html?csid=${csid}"},
+    //                linktext: "${relations.number}"
+    //            },
+    //            linksContainer: {
+    //                decorators: [
+    //                    {type: "fluid",
+    //                     func: "cspace.testComponent2",
+    //                     container: "#linksContainer",
+    //                     options: {modelPath: "relations"} }
+    //                ]
+    //            }
+    //        };
+    //        cspace.pageBuilder.uispec = {
+    //            dataEntry: testProtoTree1,
+    //            relatedRecords: testProtoTree2
+    //        };
+    //        var testModel = {
+    //            fields: {
+    //                objectTitle: "Rendered title",
+    //                briefDescription: "Rendered description",
+    //                entryMethod: ["foo", "bar", "bat"]
+    //            },
+    //            relations: {
+    //                recordType: "object",
+    //                number: "2005.1",
+    //                csid: "12345"
+    //            }
+    //        };
+    //        done = 0;
+    //        stop();
+    //        var pageBuilder = cspace.pageBuilder("#protoTreeTest", {model: testModel});
+    //    });
