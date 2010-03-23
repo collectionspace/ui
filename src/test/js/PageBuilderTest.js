4667a23504a4f4bbb20f171f09230c6f27a0115e
Anastasia Cheetham <acheetham@ocad.ca>
2010-03-23 17:31:00 +0000

    CSPACE-289: Update PageBuilder unit test to include coverage of current mini-IOC functionality; Update version numbers.

--- src/test/js/PageBuilderTest.js
+++ src/test/js/PageBuilderTest.js
@@ -41,6 +41,44 @@ cspace = cspace || {};
         }
     };
 
+    cspace.testData.dependenciesWithOptions = {
+        dateEntry: {
+            funcName: "cspace.testComponent4",
+            args: [
+                "#recordEditorContainer",   // container
+                {               // options
+                    foo: "bar",
+                    bat: "cat"
+                }  
+            ]
+        },
+        relatedRecords: {
+            funcName: "cspace.testComponent5",
+            args: [
+                "#linksContainer",           // container
+                {               // options
+                    foo: {
+                        bar: "bat",
+                        cat: "CATT"
+                    }
+                }  
+            ]
+        }
+    };
+
+    cspace.testData.dependenciesWithIOCdemands = {
+        recordEditor: {
+            funcName: "cspace.testComponent7",
+            args: [
+                "#recordEditorContainer",   // container
+                {               // options with IOC demands
+                    foo: "{pageBuilder}.options.option1",
+                    bat: "{pageBuilder}.options.option2"
+                }  
+            ]
+        }
+    };
+
     cspace.testData.testPageSpec = {
         recordEditor: {
             href: "test-data/template1.html",
@@ -73,18 +111,52 @@ cspace = cspace || {};
         var body = $("body");
         jqUnit.assertEquals("Template 1 inserted", 1, $("#testText1").length);
         jqUnit.assertEquals("Template 2 inserted", 1, $("#testText2").length);
-        jqUnit.assertEquals("Rest of doc wasn't inseted", 0, $("#shouldntbehere", body).length);
+        jqUnit.assertEquals("Rest of doc wasn't inserted", 0, $("#shouldntbehere", body).length);
         start();
+    };
+
+    cspace.testComponent4 = function (container, options) {
+        jqUnit.assertTrue("TestComponent4 instantiated", true);
+        jqUnit.assertEquals("TestComponent4 initiated with correct container", cspace.testData.dependenciesWithOptions.dateEntry.args[0], container);
+        jqUnit.assertDeepEq("TestComponent4 initiated with correct options", cspace.testData.dependenciesWithOptions.dateEntry.args[1], options);
+    };
+
+    cspace.testComponent5 = function (container, options) {
+        jqUnit.assertTrue("TestComponent5 instantiated", true);
+        jqUnit.assertEquals("TestComponent5 initiated with correct container", cspace.testData.dependenciesWithOptions.relatedRecords.args[0], container);
+        jqUnit.assertDeepEq("TestComponent5 initiated with correct options", cspace.testData.dependenciesWithOptions.relatedRecords.args[1], options);
+    };
+
+    cspace.testData.testIOCoptions = {option1: "bar", option2: "cat"};
+    cspace.testComponent6 = function (container, options) {
+        jqUnit.assertTrue("TestComponent6 instantiated", true);
+        jqUnit.assertEquals("TestComponent6 initiated with correct container", cspace.testData.dependenciesWithOptions.relatedRecords.args[0], container);
+        jqUnit.assertDeepEq("TestComponent6 initiated with correct IOC demand1", cspace.testData.testIOCoptions.option1, options.foo);
+    };
+
+    cspace.testComponent7 = function (container, options) {
+        jqUnit.assertTrue("testComponent7 instantiated", true);
+        jqUnit.assertEquals("testComponent7 initiated with correct container", cspace.testData.dependenciesWithIOCdemands.recordEditor.args[0], container);
+        jqUnit.assertDeepEq("testComponent7 initiated with correct IOC demanded option1", cspace.testData.dependenciesWithIOCdemands.recordEditor.args[1].foo, options.foo);
+        jqUnit.assertDeepEq("testComponent7 initiated with correct IOC demanded option2", cspace.testData.dependenciesWithIOCdemands.recordEditor.args[1].bat, options.bat);
+    };
+
+    cspace.testData.dependenciesWithIOCdemands = {
+        recordEditor: {
+            funcName: "cspace.testComponent7",
+            args: [
+                "#recordEditorContainer",   // container
+                {               // options with IOC demands
+                    foo: "{pageBuilder}.options.option1",
+                    bat: "{pageBuilder}.options.option2"
+                }  
+            ]
+        }
     };
 
     var pageBuilderTester = function () {
         var pageBuilderTest = new jqUnit.TestCase("PageBuilder Tests");
         
-        pageBuilderTest.test("Invocation of dependent components", function () {
-            expect(4);    // this is total num of assertions in the test components
-            cspace.pageBuilder(cspace.testData.basicDependencies);
-        });
-    
         pageBuilderTest.test("Assembly of HTML", function () {
             expect(3);
             var dependencies = {
@@ -103,11 +175,27 @@ cspace = cspace || {};
             cspace.pageBuilder(dependencies, options);
         });
 
-        pageBuilderTest.test("Constructing dependency options", function () {});
-
+        pageBuilderTest.test("Invocation of dependent components: container parameter", function () {
+            expect(4);    // this is total num of assertions in the test components
+            cspace.pageBuilder(cspace.testData.basicDependencies, cspace.testData.testIOCoptions);
+        });
+    
+        pageBuilderTest.test("Invocation of dependent components: container plus options only", function () {
+            expect(6);
+            cspace.pageBuilder(cspace.testData.dependenciesWithOptions);
+        });
+    
+        pageBuilderTest.test("Invocation of dependent components: mini IOC", function () {
+//            expect(?);
+            var pbOpts = {
+                option1: "September",
+                option2: "Toronto"
+            };
+            cspace.pageBuilder(cspace.testData.dependenciesWithIOCdemands, pbOpts);
+        });
+    
     };
     
-
     $(document).ready(function () {
         pageBuilderTester();
     });
