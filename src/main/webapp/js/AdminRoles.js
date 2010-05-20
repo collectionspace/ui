51f3171b725898c8a9f8c2f18524dab7714cb0be
Anastasia Cheetham <acheetham@ocad.ca>
2010-05-20 17:32:21 +0000

    CSPACE-1376: Update the markup, styles, etc. in preparation for proper rendering; Hook up event handlers as appropriate. Actual permissions radio buttons are not yet properly rendered.

--- src/main/webapp/js/AdminRoles.js
+++ src/main/webapp/js/AdminRoles.js
@@ -8,62 +8,26 @@ You may obtain a copy of the ECL 2.0 License at
 https://source.collectionspace.org/collection-space/LICENSE.txt
 */
 
-/*global jQuery, fluid*/
+/*global jQuery, fluid, cspace*/
+"use strict";
 
 cspace = cspace || {};
 
 (function ($, fluid) {
 
     var addNewRole = function (container, roleListEditor, domBinder, uispec) {
-        return function(e){
-            fluid.model.copyModel(roleListEditor.details.model,{
+        return function (e) {
+            fluid.model.copyModel(roleListEditor.details.model, {
                 fields: {}
             });
             roleListEditor.details.refreshView();
-            cspace.passwordValidator(container);
             roleListEditor.showDetails(true);
             roleListEditor.showNewListRow(true);
         };
     };
     
-    var restoreRoleList = function (dc, domBinder) {
-        return function () {
-            domBinder.locate("unSearchButton").hide();
-            dc.fetch(cspace.util.isLocal() ? "records/list" : null);
-        };
-    };
-
-    var submitSearch = function (domBinder, roleListEditor) {
-        return function () {
-            var query = domBinder.locate("searchField").val();
-            var model = roleListEditor.listApplier.model;
-            // TODO: Use the DC for this
-            var url = (cspace.util.isLocal() ? "data/roles/search/list.json" : "../../chain/roles/search?query=" + query);
-            $.ajax({
-                url: url,
-                type: "GET",
-                dataType: "json",
-                success: function (data, textStatus) {
-                    // that.roleListApplier.requestChange("*", data);
-                    // requestChange() to "*" doesn't work (see FLUID-3507)
-                    // the following workaround compensates:
-                    fluid.model.copyModel(model, data.results);
-                    roleListEditor.list.updateModel(model);
-                    roleListEditor.showNewListRow(false);
-                    domBinder.locate("unSearchButton").show();
-                },
-                error: function (xhr, textStatus, errorThrown) {
-                    fluid.fail("Error retrieving search results:" + textStatus);
-                }
-            });
-        };
-    };
-
     var bindEventHandlers = function (that) {
 
-        that.locate("searchButton").click(submitSearch(that.dom, that.roleListEditor));
-        that.locate("unSearchButton").click(restoreRoleList(that.roleListEditor.listDC, that.dom)).hide();
-        
         that.locate("newRole").click(addNewRole(that.container, that.roleListEditor, that.dom, that.options.uispec));
 
         that.roleListEditor.details.events.onSave.addListener(function () {
@@ -94,9 +58,6 @@ cspace = cspace || {};
             }
         },
         selectors: {
-            searchField: ".csc-role-searchField",
-            searchButton: ".csc-role-searchButton",
-            unSearchButton: ".csc-role-unSearchButton",
             messageContainer: ".csc-message-container",
             feedbackMessage: ".csc-message",
             timestamp: ".csc-timestamp",
