19ac6d4edfd80a7f5f94d1beab5733413f9b1ecb
Anastasia Cheetham <acheetham@ocad.ca>
2010-05-28 15:21:42 +0000

    NOJIRA: Updating the path to the user-admin uispec, which is used by the ListEditor tests

--- src/test/js/ListEditorTest.js
+++ src/test/js/ListEditorTest.js
@@ -16,13 +16,13 @@ var listEditorTester = function(){
     var testUISpec = {};
     $.ajax({
         async: false,
-        url: "../../main/webapp/html/uispecs/admin/uispec.json",
+        url: "../../main/webapp/html/uispecs/users/uispec.json",
         dataType: "json",
         success: function (data) {
             testUISpec = data;
         },
         error: function (xhr, textStatus, error) {
-            fluid.log("Unable to load admin uispec for testing");
+            fluid.log("Unable to load users uispec for testing");
         }
     });
     var baseTestOpts = {
