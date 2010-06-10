24e4dbdeb667632e2fc598187bea7001a9b71f2e
Anastasia Cheetham <acheetham@ocad.ca>
2010-06-10 17:36:20 +0000

    CSPACE-2038: Committing Laurel's patch to upgrade the JavaScript unit testing framework to the latest version.

--- src/test/js/NumberPatternChooserTest.js
+++ src/test/js/NumberPatternChooserTest.js
@@ -11,33 +11,32 @@ https://source.collectionspace.org/collection-space/LICENSE.txt
 /*global jQuery, jqUnit, cspace*/
 
 (function ($) {
-    
-    $(document).ready(function () {
-        var chooser;
-        var model = {
+	
+    var model = {
 		    "list": ["val1", "val2", "val3"],
 			"samples": ["sample1", "sample2", "sample3"],
 			"names": ["name1", "name2", "name3"]
+    };
+    
+    var testOpts = {
+    	model: model,
+        templateUrl: "../../main/webapp/html/NumberPatternChooser.html"
+    };
+
+	
+    var numberPatternChooserTest = new jqUnit.TestCase("NumberPatternChooser Tests");
+
+    numberPatternChooserTest.test("Creation", function () {
+        var numberPatternChooser;
+        testOpts.listeners = {
+            afterRender: function () {
+            	jqUnit.isVisible("Number pattern chooser button should be visible", $(".csc-numberPatternChooser-button"));
+                start();
+            }
         };
-        var options = {
-                listeners: {
-                    afterRender: function () {
-                        start();
-                    }
-                },
-                model: model,
-                templateUrl: "../../main/webapp/html/NumberPatternChooser.html"
-            };
-
-        var mySetup = function () {
-            stop();
-            chooser = cspace.numberPatternChooser(".info-value", options);
-        };
-
-        var numberPatternChooserTest = new jqUnit.TestCase("NumberPatternChooser Tests", mySetup);
-
-	    numberPatternChooserTest.test("Construction", function () {
-            jqUnit.isVisible("Number pattern chooser button should be visible", $(".csc-numberPatternChooser-button"));
-	    });
+        
+        numberPatternChooser = cspace.numberPatternChooser(".info-value", testOpts);
+        stop();
     });
+
 })(jQuery);
