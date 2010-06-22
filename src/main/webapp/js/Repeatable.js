4c2e3d9517167708e54b7b5ef9a6a1c7d4924083
Michelle D'Souza <michelled33@gmail.com>
2010-06-22 20:58:18 +0000

    CSPACE-1278: Changed the repeatable component to generate the markup it requires in order to reduce the expectation on implementors when configuring which fields are repeatable. Added a convenience function for creating a repeatable component from an element. Simplified the markup for brief description as an example of minimal semantic markup - requires styling. Simplified the tests to make them easier to debug when one of the breaks.

--- src/main/webapp/js/Repeatable.js
+++ src/main/webapp/js/Repeatable.js
@@ -16,6 +16,7 @@ cspace = cspace || {};
 (function ($, fluid) {
     
     // TODO: Account for an elPath into the model that points to undefined: not known whether it is a simple field or an object/row.
+    //       We need to write a test for this but I think it is fixed now
     var addRow = function (fields) {
         // TODO: It's not yet sure whether or not a simple object like this will work for
         //       groups of fields - we'll have to test against the server when it's supported
@@ -33,18 +34,20 @@ cspace = cspace || {};
         });
         
         // Make a change request to add an extra row.
-        that.locate("addButton").click(function () {                        
+        that.locate("add").click(function () {                        
             that.applier.requestChange(elPath, addRow(fluid.model.getBeanValue(that.model, elPath)));
             that.refreshView();
             that.events.afterAdd.fire();
         });
         
-        that.locate("deleteButton").click(function () {
+        that.locate("remove").click(function () {
             that.events.afterDelete.fire();
         });
     };
     
     var renderPage = function (that) {
+        that.options.renderOptions.cutpoints.push({id: "repeat:", selector: that.options.selectors.repeat});
+        
         // TODO: We should pass the args to the expander that are consistent with generic expanders.
         var tree = fluid.invokeGlobalFunction(that.options.expander, [that.options.protoTree, that]);
         $.extend(true, that.options.renderOptions, {
@@ -74,13 +77,42 @@ cspace = cspace || {};
         }
     };
 
-    cspace.repeatable = function (container, options) {
+    /**
+     * @node content node that will contain primary and delete
+     */
+    var addPrimaryAndDelete = function (that, node) {
+        // TODO: we need to programatically generate the 'name' attribute since we need more then one group of radio buttons on a page. 
+        var primary = $("<input class=\"csc-repeatable-primary\" type=\"radio\" name=\"primary\" />");
+        var remove = $("<input class=\"csc-repeatable-delete\" type=\"button\" />");
+                
+        if (node[0].tagName === "TR") {
+            primary.wrap("<td />");
+            remove.wrap("<td />");
+        }
         
+        if (that.locate("primary").length === 0) {
+            node.prepend(primary);
+        }
+        
+        if (that.locate("remove").length === 0) {
+            node.append(remove);
+        }
+                
+    };
+    
+    
+    /**
+     * Repeatable is a markup generating component. If it does not find things in the markup that are specified 
+     * in the default selectors, it generates nodes appropriately and puts the default selector classes into the markup.
+     *  
+     */
+    cspace.repeatable = function (container, options) {        
         var that = fluid.initView("cspace.repeatable", container, options);
         
         that.applier = that.options.applier;
         that.model = that.options.model;
         prepareModel(that.model, that.options.elPath, that.applier);
+        that.options.generateMarkup(that);
         
         that.refreshView = function () {
             renderPage(that);
@@ -90,6 +122,26 @@ cspace = cspace || {};
         return that;
     };
     
+    cspace.repeatable.generateMarkup = function (that) {
+        // Check for the add button and generate it if required 
+        if (that.locate("add").length === 0) {
+            // TODO: i18n the string
+            that.container.prepend("<input class=\"csc-repeatable-add\" type=\"button\" value=\"+ Field\" />");
+        }
+        
+        var node = that.locate("repeat");
+        // TODO: check that we have a repeating node - if not what should we do? grab the first thing? grab everything?
+        
+        if (node[0].tagName !== "TR" && node[0].tagName !== "LI") {
+            node.wrap("<ul><li class=\"csc-repeatable-repeat\"/></ul>");            
+            // TODO: there is probably a bug here when the 'repeat' selector is overridden - write a test to prove this
+            node.removeClass("csc-repeatable-repeat");
+            node = node.parent("li");    
+        }
+
+        addPrimaryAndDelete(that, node);        
+    };
+
     // TODO: This is a cspace specific function, we need to take it out to utilities for example.
     cspace.repeatable.extendDecoratorOptions = function (options, parentComponent) {
         $.extend(true, options, {
@@ -97,15 +149,27 @@ cspace = cspace || {};
             model: parentComponent.model,
             renderOptions: {
                 cutpoints: cspace.renderUtils.cutpointsFromUISpec(options.protoTree)
-            },
-            expander: "cspace.renderUtils.expander"
+            }
         });
+    };
+
+    cspace.repeatable.expander = function (preProtoTree, that) {
+        var rowSelector = "repeat:";
+        var protoTree = {};
+        protoTree[rowSelector] = {
+            children : [preProtoTree]
+        };
+        
+        // TODO: this depends on cspace utilities - once the functionality is in infusion remove the dependency
+        return cspace.renderUtils.expander(protoTree, that);
     };
     
     fluid.defaults("cspace.repeatable", {
         selectors: {
-            addButton: ".csc-repeatable-add",
-            deleteButton: ".csc-repeatable-delete"
+            add: ".csc-repeatable-add",
+            remove: ".csc-repeatable-delete",
+            primary: ".csc-repeatable-primary",
+            repeat: ".csc-repeatable-repeat"     
         },
         events: {
             afterRender: null,
@@ -114,14 +178,27 @@ cspace = cspace || {};
         },         
         applier: null,      // Applier for the main record that cspace.repeatable belongs to. REQUIRED
         model: null,        // Model for the main record that cspace.repeatable belongs to. REQUIRED
-        elPath: "",         // Path into the model that points to the field/collection of fields to be repeated.
+        elPath: "items",         // Path into the model that points to the collection of fields to be repeated - it should be an array.
         protoTree: {},      // A dehydrated tree that will be expanded by the expander and rendered in the component's refreshView.
-        expander: null,     // Expands the protoTree to take form of the componentTree comprehendable by the renderer.
+        expander: "cspace.repeatable.expander",     // Expands the protoTree to take form of the componentTree comprehendable by the renderer.
         renderOptions: {    // Render options that (including cutpoints) that will be passed to the renderer in the component's refreshView.
-            autoBind: true,
-            // debugMode: true,
-            cutpoints: []
-        } 
+            autoBind: true
+           // debugMode: true
+        },
+        generateMarkup: cspace.repeatable.generateMarkup   // TODO: change this to a function name instead of a function
     });
     
+    /**
+     * @element a jqueryable selector
+     */
+    cspace.makeRepeatable = function (element, options) {
+        element = $(element);
+        element.addClass("csc-repeatable-repeat");
+        element.wrap("<div />");
+        
+        return cspace.repeatable(element.parent("div"), options);
+    };
+
+    cspace.makeRepeatable.extendDecoratorOptions = cspace.repeatable.extendDecoratorOptions;
+
 })(jQuery, fluid);
--- src/test/js/RepeatableTest.js
+++ src/test/js/RepeatableTest.js
@@ -11,114 +11,266 @@ https://source.collectionspace.org/collection-space/LICENSE.txt
 /*global jqUnit, jQuery, cspace, fluid, start, stop, ok, expect*/
 "use strict";
 
-var repeatableTester = function () {
-    var testUISpec = {};
-    var testModel = {};
-    var repeatable;
-    jQuery.ajax({
-        async: false,
-        url: "../../main/webapp/html/uispecs/objects/uispec.json",
-        dataType: "json",
-        success: function (data) {
-            testUISpec = data["recordEditor"][".csc-object-identification-brief-description-repeated-container"].decorators[0];
-        },
-        error: function (xhr, textStatus, error) {
-            fluid.log("Unable to load object uispec for testing");
-        }
-    });
-    jQuery.ajax({
-        async: false,
-        url: "../../main/webapp/html/data/objects/1984.068.0335b.json",
-        dataType: "json",
-        success: function (data) {
-            testModel = data;
-        },
-        error: function (xhr, textStatus, error) {
-            fluid.log("Unable to load object data for testing");
-        }
-    });
+var repeatableTester = function ($) {
+    var briefDescUISpec;
+    var briefDescModel;
+
+    var repeatableTest = new jqUnit.TestCase("Repeatable Tests");
     
-    var repeatableTest = new jqUnit.TestCase("Repeatable Tests", function () {
-        repeatableTest.fetchTemplate("../../main/webapp/html/ObjectEntryTemplate.html", ".csc-object-entry-template");
-    });
-    
-    var setupRepeatable = function (options) {
-        var model = {};
-        fluid.model.copyModel(model, testModel);
-        var applier = fluid.makeChangeApplier(model);
-        var opts = testUISpec.options;
-        opts.model = model;
-        opts.applier = applier;
-        opts.renderOptions = {
-            cutpoints: cspace.renderUtils.cutpointsFromUISpec(testUISpec.options.protoTree)
+    var basicSetup = function (options) {
+        options = options || {};
+        var container = options.container || "#simpleFieldContainer";
+        var cutpoints = options.cutpoints || [{
+                id: "myTextField",
+                selector: ".cst-simpleTestField"
+            }
+        ];
+        var model = options.model || {
+            myTexts: [
+                {
+                    myText: options.text || ""
+                }
+            ]
         };
-        opts.expander = "cspace.renderUtils.expander";
-        jQuery.extend(true, opts, options); 
-        return cspace.repeatable(".csc-object-identification-brief-description-repeated-container", opts);
+
+        return cspace.repeatable(container, {
+                model: model,
+                elPath: "myTexts",
+                applier: fluid.makeChangeApplier(model),
+                protoTree: {
+                    "myTextField": "${myTexts.0.myText}"
+                },
+                renderOptions: {
+                    cutpoints: cutpoints
+                }
+            }
+        );        
+    };
+
+    // TODO: This is related to testing cspace brief description
+    //       When the Repeatable component moves to Infusion this part of the test file will likely remain in the cspace code base.
+    var setupRepeatableWithBriefDesc = function () {
+        if (!briefDescUISpec) {
+            jQuery.ajax({
+                async: false,
+                url: "../../main/webapp/html/uispecs/objects/uispec.json",
+                dataType: "json",
+                success: function (data) {
+                    briefDescUISpec = data.recordEditor[".csc-object-identification-brief-description"].decorators[0];
+                },
+                error: function (xhr, textStatus, error) {
+                    fluid.log("Unable to load object uispec for testing");
+                }
+            }); 
+        }
+        
+        if (!briefDescModel) {
+            jQuery.ajax({
+                async: false,
+                url: "../../main/webapp/html/data/objects/1984.068.0335b.json",
+                dataType: "json",
+                success: function (data) {
+                    briefDescModel = data;
+                },
+                error: function (xhr, textStatus, error) {
+                    fluid.log("Unable to load object data for testing");
+                }
+            });
+            
+        }
+
+        var model = {}, options = {};
+        fluid.model.copyModel(model, briefDescModel);
+
+        var opts = {
+            model : model,
+            applier : fluid.makeChangeApplier(model),
+            renderOptions : {
+                cutpoints: cspace.renderUtils.cutpointsFromUISpec(briefDescUISpec.options.protoTree)
+            }
+        };
+
+        jQuery.extend(true, options, briefDescUISpec.options, opts); 
+
+        repeatableTest.fetchTemplate("../../main/webapp/html/ObjectEntryTemplate.html", ".csc-object-identification-brief-description");
+        return cspace.makeRepeatable(".csc-object-identification-brief-description", options);
     };
     
-    repeatableTest.test("Initialization", function () {        
-        repeatable = setupRepeatable();
-        jqUnit.assertValue("Component was initialized", repeatable);
+    var basicMarkupGenerateTest = function (container, repeatEl, text) {
+        // 10 asserts
+        
+        var ul = $("ul", container);
+        jqUnit.exists("The ul has been generated inside container", ul);
+       
+        var li = $("li", ul);
+        jqUnit.exists("The li has been generated inside the ul", li);
+        jqUnit.assertTrue("The li has the repeat selector", li.hasClass("csc-repeatable-repeat"));
+        
+        var field = $(repeatEl, li);
+        jqUnit.exists("The field has been placed inside the li", field);
+        jqUnit.assertFalse("The field no longer has the repeat selector", field.hasClass("csc-repeatable-repeat"));
+        jqUnit.assertEquals("The original model was rendered ", text, field.val());
+        jqUnit.exists("The delete has been generated inside the li", $(".csc-repeatable-delete", li));
+        jqUnit.exists("The primary has been generated inside the li", $(".csc-repeatable-primary", li));
+
+        var addButton = $(".csc-repeatable-add", container);
+        jqUnit.exists("The add button has been generated inside container", addButton);
+        jqUnit.notExists("The add button is not in the ul", $(".csc-repeatable-add", ul));        
+    };
+    
+    repeatableTest.test("Markup Generation for Basic Component", function () {
+        expect(10);
+        var myRepeatable = basicSetup({text: "blue"});
+        
+        basicMarkupGenerateTest(myRepeatable.container, ".cst-simpleTestField", "blue");
     });
-    repeatableTest.test("Add Functionality", function () {        
-        repeatable = setupRepeatable();
-        jqUnit.assertEquals("Model is of length 1 initially", 1, fluid.model.getBeanValue(repeatable.model, repeatable.options.elPath).length);
-        repeatable.locate("addButton").click();
-        jqUnit.assertEquals("After clicking 'add', model is now of length 2", 2, fluid.model.getBeanValue(repeatable.model, repeatable.options.elPath).length);
+   
+    repeatableTest.test("Markup Generation For a Table", function () {
+        expect(7);
+        var myRepeatable = basicSetup({
+            container: "#tableContainer", 
+            cutpoints: [{
+                id: "myTextField",
+                selector: ".csc-repeatable-repeat input"
+            }], 
+            text: "circle"
+        });
+        var container = myRepeatable.container;
+        
+        var ul = $("ul", container);
+        jqUnit.notExists("No ul has been generated inside container", ul);
+       
+        var tr = $("tr", container);
+        jqUnit.assertTrue("The tr has the repeat selector", tr.hasClass("csc-repeatable-repeat"));
+        jqUnit.exists("The delete has been generated inside the table row", $(".csc-repeatable-delete", tr));
+        jqUnit.exists("The primary has been generated inside the table row", $(".csc-repeatable-primary", tr));
+        jqUnit.assertEquals("The original model was rendered ", "circle", $(".cst-tableTestField", tr).val());
+    
+        var addButton = $(".csc-repeatable-add", container);
+        jqUnit.exists("The add button has been generated inside container", addButton);
+        var table = $("table", container);
+        jqUnit.notExists("The add button is not in the table", $(".csc-repeatable-add", table));
     });
     
-    repeatableTest.test("Add Functionality + applier/model consistency", function () {        
-        repeatable = setupRepeatable();        
-        jqUnit.assertEquals("Model is of lenth 1 initially", 1, fluid.model.getBeanValue(repeatable.model, repeatable.options.elPath).length);
-        jqUnit.assertEquals("Initially, value matched model", "This is brief description.", jQuery(".csc-object-identification-brief-description").val());
-        jQuery(".csc-object-identification-brief-description").val("test").change();
-        jqUnit.assertEquals("Before adding a row, first value is changed to 'test'", "test", jQuery(".csc-object-identification-brief-description").val());
-        jqUnit.assertDeepEq("After changing field to 'test', model should be 'test", {briefDescription: "test"}, fluid.model.getBeanValue(repeatable.model, repeatable.options.elPath)[0]);
-        repeatable.applier.modelChanged.addListener("*", function () {
-            jqUnit.assertEquals("After clicking 'add', model is now of length 2", 2, fluid.model.getBeanValue(repeatable.model, repeatable.options.elPath).length);
-            jqUnit.assertEquals("After adding a row, first value is still 'test'", "test", jQuery(".csc-object-identification-brief-description").eq(0).val());
-            jqUnit.assertDeepEq("After adding a row, model for first field should be 'test", {briefDescription: "test"}, fluid.model.getBeanValue(repeatable.model, repeatable.options.elPath)[0]);
+    repeatableTest.test("Multi rows in the model", function () {     
+        expect(4);
+        var myRepeatable = basicSetup({model: {
+                myTexts: [
+                    {
+                        myText: "cat"
+                    },
+                    {
+                        myText: "dog"
+                    },
+                    {
+                        myText: "fish"
+                    }
+
+                ]
+            }}
+        );
+
+        var li = $("li", myRepeatable.container);
+        jqUnit.assertEquals("There are three items rendered", 3, li.length);
+        jqUnit.assertEquals("cat is the first item", "cat", $(".cst-simpleTestField", li.eq(0)).val());
+        jqUnit.assertEquals("dog is the second item", "dog", $(".cst-simpleTestField", li.eq(1)).val());
+        jqUnit.assertEquals("fish is the first item", "fish", $(".cst-simpleTestField", li.eq(2)).val());
+
+    });
+  
+    repeatableTest.test("Add Functionality", function () {     
+        expect(3);
+        var myRepeatable = basicSetup();
+        
+        jqUnit.assertEquals("Model is of length 1 initially", 1, fluid.model.getBeanValue(myRepeatable.model, myRepeatable.options.elPath).length);
+        myRepeatable.locate("add").click();
+        jqUnit.assertEquals("After clicking 'add', model is now of length 2", 2, fluid.model.getBeanValue(myRepeatable.model, myRepeatable.options.elPath).length);
+
+        var li = $("li", myRepeatable.container);
+        jqUnit.assertEquals("There should be 2 items", 2, li.length);
+    });
+
+    repeatableTest.test("Add Functionality + applier/model consistency", function () {   
+        expect(7);
+        var myRepeatable = basicSetup({text: "thyme"});  
+        var field = $(".cst-simpleTestField");
+        var expectedModel = {myText: "oregano"};
+        
+        jqUnit.assertEquals("Model is of lenth 1 initially", 1, fluid.model.getBeanValue(myRepeatable.model, myRepeatable.options.elPath).length);
+        jqUnit.assertEquals("Initially, value matched model", "thyme", field.val());
+        field.val("oregano").change();
+        
+        jqUnit.assertEquals("Before adding a row, first value is changed to 'oregano'", "oregano", $(".cst-simpleTestField").val());
+        jqUnit.assertDeepEq("After changing field to 'oregano', model should be 'oregano", expectedModel, fluid.model.getBeanValue(myRepeatable.model, myRepeatable.options.elPath)[0]);
+        myRepeatable.applier.modelChanged.addListener("*", function () {
+            jqUnit.assertEquals("After clicking 'add', model is now of length 2", 2, fluid.model.getBeanValue(myRepeatable.model, myRepeatable.options.elPath).length);
+            jqUnit.assertEquals("After adding a row, first value is still 'oregano'", "oregano", field.val());
+            jqUnit.assertDeepEq("After adding a row, model for first field should be 'oregano", expectedModel, fluid.model.getBeanValue(myRepeatable.model, myRepeatable.options.elPath)[0]);
         });
-        repeatable.locate("addButton").click();        
+        myRepeatable.locate("add").click();
+        
     });
 
     repeatableTest.test("Add functionality when model initially empty", function () {
-        var oldTestModel = {};
-        fluid.model.copyModel(oldTestModel, testModel);
-        fluid.clear(testModel);        
+        expect(2);
 
-        repeatable = setupRepeatable();
-        jqUnit.assertEquals("When initialized with an empty  model, model length should be 1", 1, fluid.model.getBeanValue(repeatable.model, repeatable.options.elPath).length);
-        repeatable.applier.modelChanged.addListener("*", function () {
-            jqUnit.assertEquals("After clicking 'add', model is now of length 2", 2, fluid.model.getBeanValue(repeatable.model, repeatable.options.elPath).length);
+        var myRepeatable = basicSetup({model: {}});  
+        jqUnit.assertEquals("When initialized with an empty  model, model length should be 1", 1, fluid.model.getBeanValue(myRepeatable.model, myRepeatable.options.elPath).length);
+        myRepeatable.applier.modelChanged.addListener("*", function () {
+            jqUnit.assertEquals("After clicking 'add', model is now of length 2", 2, fluid.model.getBeanValue(myRepeatable.model, myRepeatable.options.elPath).length);
         });
-        repeatable.locate("addButton").click();
-        fluid.clear(testModel);
-        fluid.model.copyModel(testModel, oldTestModel);
+        myRepeatable.locate("add").click();
     });
     
-    repeatableTest.test("Delete Functionality", function () {
-        // TODO: Delete is not yet implemented, so only the 'after clicking add' assert will run currently
-        //       An expect() would catch this
-        repeatable = setupRepeatable();
-        repeatable.events.afterDelete.addListener(function () {
-            jqUnit.assertEquals("After delete of only item, model is still of length 1", 1, fluid.model.getBeanValue(repeatable.model, repeatable.options.elPath).length);
-        }, "deletePrimary");
-        jQuery(".csc-repeatable-delete").eq(0).click();
-        repeatable.events.afterDelete.removeListener("deletePrimary");
-        repeatable.applier.modelChanged.addListener("*", function () {
-            jqUnit.assertEquals("After clicking 'add', model is now of length 2", 2, fluid.model.getBeanValue(repeatable.model, repeatable.options.elPath).length);
-        });
-        repeatable.locate("addButton").click();
-        repeatable.events.afterDelete.addListener(function () {
-            jqUnit.assertEquals("After deleting non-primary item, model is of length 1", 1, fluid.model.getBeanValue(repeatable.model, repeatable.options.elPath).length);
-        }, "deleteNonPrimary");
-        jQuery(".csc-repeatable-delete").eq(1).click();
-        repeatable.events.afterDelete.removeListener("deleteNonPrimary");
+    // TODO: Need to write a test where the markup isn't generated
+
+    repeatableTest.test("Make repeatable with simple field", function () {
+        expect(12);
+        
+        var model = {
+            myTexts: [
+                {
+                    myText: "Bruges"
+                }
+            ]
+        };
+
+        var options = {
+            model: model,
+            applier: fluid.makeChangeApplier(model),
+            protoTree: {
+                "myTextField": "${myTexts.0.myText}"
+            },
+            renderOptions: {
+                cutpoints: [{
+                    id: "myTextField",
+                    selector: ".cst-simpleTestFieldNoContainer"
+                }]
+            }
+        };
+        
+        var myRepeatable = cspace.makeRepeatable(".cst-simpleTestFieldNoContainer", options);
+        
+        var container = myRepeatable.container;
+        jqUnit.assertFalse("The container is not the element we are repeating", container.hasClass("cst-simpleTestFieldNoContainer"));
+        jqUnit.assertEquals("the container is a div ", "DIV", container[0].tagName);
+        
+        basicMarkupGenerateTest(container, ".cst-simpleTestFieldNoContainer", "Bruges");    
+    });
+
+    
+    
+    // TODO: write a test which has a couple of fields in a div that is to be repeated
+    // TODO: write a test where the markup uses an li
+    
+    repeatableTest.test("Init test for brief description", function () {
+        expect(10);
+        var myRepeatable = setupRepeatableWithBriefDesc();
+        basicMarkupGenerateTest(myRepeatable.container, ".csc-object-identification-brief-description", "This is brief description.");
+
     });
 };
 
 (function () {
-    repeatableTester();
+    repeatableTester(jQuery);
 }());
