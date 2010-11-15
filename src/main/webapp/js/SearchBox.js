81909d2bb596bbb6a2d372eb1bc7a8c6b9a502a4
Yura Zenevich <yzenevich@ocad.ca>
2010-11-15 16:57:32 +0000

    CSPACE-3187: Added more comments to the searchBox component.

--- src/main/webapp/js/SearchBox.js
+++ src/main/webapp/js/SearchBox.js
@@ -28,10 +28,18 @@ cspace = cspace || {};
     };
     
     fluid.defaults("cspace.searchBox", {
-        operations: ["create", "read", "update", "delete", "list"],
-        method: "OR",
+        operations: ["create", "read", "update", "delete", "list"], // A list of allowing permissions that will
+                                                                    // instruct the searchBox to render accordingly 
+                                                                    // (Used by cspace.permissions.manager).
+        method: "OR",   // A method type that indicates how to resolve against the "operations" list: as an OR or
+                        // as an AND (Used by cspace.permissions.manager). If it is "OR", permissions manager (PM) 
+                        // will "allow" the resource if any of the operations matche to the user permissions. If 
+                        // it is "AND", PM will "allow" the resource only if all operations match the set of user 
+                        // permissions.  
         invokers: {
-            provideResolverGetConfig: {
+            provideResolverGetConfig: { // A public searchBox's provideResolverGetConfig method that returns an 
+                                        // array of strategies to be used by fluid.model.getBeanValue to resolve
+                                        // the values in the model against the schema and permissions. 
                 funcName: "cspace.searchBox.provideResolverGetConfig",
                 args: {
                     permissions: "{searchBox}.options.permissions",
@@ -44,6 +52,7 @@ cspace = cspace || {};
     });
     
     var bindEvents = function (that) {
+        // Bind a click event on search button to trigger searchBox's navigateToSearch
         that.locate("searchButton").click(that.navigateToSearch);
     };
     
@@ -54,19 +63,25 @@ cspace = cspace || {};
     };
     
     var setupSearchBox = function (that) {
+        // Check whether the components needs to be rendered as part of its initialization.
         if (that.options.selfRender) {
             that.refreshView();
         }
     };
     
+    // Actual implementation of the searchBox component's creator function.
     cspace.searchBoxImpl = function (container, options) {
-        var that = fluid.initRendererComponent("cspace.searchBoxImpl", container, options);        
+        // Initialize searchBox component as a renderer component.
+        var that = fluid.initRendererComponent("cspace.searchBoxImpl", container, options);
         that.model = that.options.model;
+        // Initialize component's dependants if any and component's invokers.
         fluid.initDependents(that);
         setupSearchBox(that);
         return that;
     };
     
+    // A public function that is called as searchBox's navigateToSearch method and redirects to
+    // the search results page.
     cspace.searchBox.navigateToSearch = function (that) {
         var url = fluid.stringTemplate(that.options.searchUrl, {
             recordtype: that.locate("recordTypeSelect").val(),
@@ -75,20 +90,30 @@ cspace = cspace || {};
         window.location = url;
     };
     
+    // A public funtion that returns a defined array of strategies used by fluid.model.getBeanValue
+    // to resolve values in the model based on permissions and schema. 
     cspace.searchBox.provideResolverGetConfig = function (options) {
         return [cspace.util.censorWithSchemaStrategy(options)];
     };
     
+    // A public function that is called as searchBox's refreshView method, renders the component and
+    // binds the event handlers.
     cspace.searchBox.refreshView = function (that) {
+        // No need to pass arguments to treeBuilder since it is an invoker.
         var tree = that.treeBuilder();
+        // Render the component.
         that.render(tree);
+        // Bind event handlers.
         bindEvents(that);
     };
     
+    // A public function that is called as searchBox's treeBuilder method and builds a component tree.
     cspace.searchBox.modelToTree = function (model, options) {
         var tree = {
             searchButton: {
-                messagekey: "searchButtonText"
+                messagekey: "searchButtonText"  // Key into the strings structure in component's options 
+                                                // that will instruct the renderer to use the particular
+                                                // string for that rendered element.
             },
             searchQuery: {},
             recordTypeSelectLabel: {
@@ -96,10 +121,13 @@ cspace = cspace || {};
             },
             recordTypeSelect: {
                 optionlist: cspace.util.elStylefy(options.elPath),
+                // NOTE: ONLY CALL fluid.model.getBeanValue WITH DEFINED resolverGetConfig STRATEGY FOR 
+                // SCHEMA AND PERMISSIONS RESOLUTION.
                 optionnames: buildOptionNames(fluid.model.getBeanValue(model, options.elPath, options.resolverGetConfig), options.strings),
                 selection: ""
             }
         };
+        // Adding all custom components styles.
         fluid.each(tree, function (child, key) {
             child.decorators = [{
                 type: "addClass",
@@ -111,23 +139,24 @@ cspace = cspace || {};
     
     fluid.defaults("cspace.searchBoxImpl", {
         mergePolicy: {
-            model: "preserve"
+            model: "preserve"       // If the model is passed to the component, preserve the original 
+                                    // object on options merging.
         },
-        elPath: "recordlist",
-        selectors: {
+        elPath: "recordlist",       // Configurable path of interest into the model. 
+        selectors: {                // Set of selectors that the component is interested in rendering.
             recordTypeSelect: ".csc-searchBox-selectRecordType",
             recordTypeSelectLabel: ".csc-searchBox-selectRecordTypeLabel",
             searchQuery: ".csc-seachBox-query",
             searchButton: ".csc-seachBox-button"
         },
-        styles: {
+        styles: {                   // Set of styles that the component will be adding onto selectors.
             searchBox: "cs-searchBox",
             recordTypeSelect: "cs-searchBox-selectRecordType",
             recordTypeSelectLabel: "cs-searchBox-selectRecordTypeLabel",
             searchQuery: "cs-seachBox-query",
             searchButton: "cs-seachBox-button"
         },
-        strings: {
+        strings: {                  // List of strings that the component will render (for l10n and i18n).
             searchButtonText: "Search",
             recordTypeSelectLabel: "",
             cataloging: "Cataloging",
@@ -141,32 +170,34 @@ cspace = cspace || {};
             "location": "Storage Location",
             contact: "Contact" 
         },
-        model: {},
-        resolverGetConfig: null,
-        invokers: {
-            treeBuilder: {
+        model: {},                  // A default data model object.
+        resolverGetConfig: null,    // An option used by initRendererComponent as a strategy when 
+                                    // resolving values in the model (through fluid.model.getBeanValue)
+        invokers: {                 // Component's public functions with arguments that are resolved at the time of invokation.
+            treeBuilder: {          // A public method that builds component tree for rendering.
                 funcName: "cspace.searchBox.modelToTree",
                 args: ["{searchBoxImpl}.model", "{searchBoxImpl}.options"]
             },
-            refreshView: {
+            refreshView: {          // A public method that renders the component and binds event handlers anew.
                 funcName: "cspace.searchBox.refreshView",
                 args: ["{searchBoxImpl}"]
             },
-            navigateToSearch: {
+            navigateToSearch: {     // A public method that builds search page's url and navigates to that page.
                 funcName: "cspace.searchBox.navigateToSearch",
                 args: ["{searchBoxImpl}"]
             }
         },
-        selfRender: false,
-        searchUrl: "findedit.html?recordtype=%recordtype&keywords=%keywords",
-        resources: {
+        selfRender: false,          // An options that indicates whether the component needs to render on initialization.
+        searchUrl: "findedit.html?recordtype=%recordtype&keywords=%keywords",   // Search page's url template.
+        resources: {                // A set of resources that will get resolved and fetched at some point during initialization.
             template: {
                 expander: {
                     type: "fluid.deferredInvokeCall",
                     func: "cspace.specBuilder",
                     args: {
                         forceCache: true,
-                        fetchClass: "fastTemplate",
+                        fetchClass: "fastTemplate", // Class name that indicates to pageBuilder that the 
+                                                    // template is necessary on page's initialization. 
                         url: "%webapp/html/SearchBoxTemplate.html"
                     }
                 }
@@ -174,6 +205,7 @@ cspace = cspace || {};
         }
     });
     
+    // This funtction executes on file load and starts the fetch process of component's template.
     fluid.fetchResources.primeCacheFromResources("cspace.searchBoxImpl");
     
 })(jQuery, fluid);
