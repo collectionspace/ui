0ac26da6961214f92827f39d5769ec46d9d0e8f3
Yura Zenevich <yzenevich@ocad.ca>
2011-06-06 20:33:27 +0000

    CSPACE-3735: Once there are new changes to the record the message bar now is automatically hidden.

--- src/main/webapp/js/RecordEditor.js
+++ src/main/webapp/js/RecordEditor.js
@@ -117,6 +117,7 @@ cspace = cspace || {};
         
         that.options.applier.modelChanged.addListener("fields", function (model, oldModel, changeRequest) {
             processChanges(that, true);
+            that.options.messageBar.hide();
         });
 
         that.options.dataContext.events.onError.addListener(makeDCErrorHandler(that));
