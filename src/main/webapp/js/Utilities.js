d7af1f17f4e424c5fc9fe3ac010caf8c9f1e5501
Yura Zenevich <yzenevich@ocad.ca>
2011-06-09 16:48:14 +0000

    CSPACE-4065: Fixed toggle headers to be accessible with keyboard.

--- src/main/webapp/js/Utilities.js
+++ src/main/webapp/js/Utilities.js
@@ -1128,7 +1128,7 @@ fluid.registerNamespace("cspace.util");
         var that = fluid.initView("cspace.util.togglable", container, options);
         
         that.locate("header").addClass(that.options.styles[that.options["default"]])
-            .addClass(that.options.styles.header);
+            .addClass(that.options.styles.header).fluid("tabbable");
         
         that.getNext = function (source) {
             var next = source.next(that.options.selectors.togglable);
@@ -1138,13 +1138,30 @@ fluid.registerNamespace("cspace.util");
             // Assume that the source is last in the block.
             return source.parent().next(that.options.selectors.togglable);
         };
-            
-        that.container.delegate(that.options.selectors.header, "click", function () {
-            var source = $(this);
+        
+        var toggle = function (source) {
             that.getNext(source).toggle();
             source.toggleClass(that.options.styles.expanded);
             source.toggleClass(that.options.styles.collapsed);
             return false;
+        };
+        
+        // Need clean listners to cover the case when the markup was rendered.
+        // Listeners from wiped markup retain and thus double.
+        that.container.undelegate(that.options.selectors.header, "keyup");
+        that.container.undelegate(that.options.selectors.header, "click");
+        // Using fluid.activatable didn't work + need to delegate since the
+        // markup can get re-rendered.
+        that.container.delegate(that.options.selectors.header, "keyup", function (event) {
+            var key = cspace.util.keyCode(event);
+            if (key !== $.ui.keyCode.ENTER && key !== $.ui.keyCode.SPACE) {
+                return;
+            }
+            return toggle($(this));
+        });
+            
+        that.container.delegate(that.options.selectors.header, "click", function () {
+            return toggle($(this));
         });
         
         return that;
