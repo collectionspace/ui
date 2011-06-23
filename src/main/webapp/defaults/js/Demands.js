db1f4c4bbbdbfef6673d61f5bf17059911afd6f1
Yura Zenevich <yzenevich@ocad.ca>
2011-06-23 01:32:00 +0000

    NOJIRA: Disabling logging in the v1.8 branch.

--- src/main/webapp/defaults/js/Demands.js
+++ src/main/webapp/defaults/js/Demands.js
@@ -13,6 +13,8 @@ https://source.collectionspace.org/collection-space/LICENSE.txt
 
 (function ($, fluid) {
     
+    fluid.setLogging(false);
+    
     fluid.registerNamespace("cspace");
     
     cspace.includeLocalDemands = function () {
