6ec7e7e5257c7c61193ca620a097d3590f317b9b
Alexey Novak <abovebluesky@gmail.com>
2012-01-20 15:10:35 -0500

    CSPACE-3786: Fixed issues based on yzen pull request comments. Changed grammar in one of the places. Refactored remove function which will be changed depending on the record type. Changed test cases.

--- src/main/webapp/defaults/js/RecordEditor.js
+++ src/main/webapp/defaults/js/RecordEditor.js
@@ -265,7 +265,7 @@ cspace = cspace || {};
         return that;
     };
     
-    cspace.recordEditor.remove = function (that) {
+    cspace.recordEditor.removeWithCheck = function (that) {
         // If our record is used by any other record then we do not want to allow to
         // delete it. Just notify a user about it.
         if (!!that.model.refobjs && that.model.refobjs.length > 0) {
@@ -283,26 +283,30 @@ cspace = cspace || {};
                 parentBundle: that.options.parentBundle
             });
         } else {
-            that.confirmation.open("cspace.confirmation.deleteDialog", undefined, {
-                listeners: {
-                    onClose: function (userAction) {
-                        if (userAction === "act") {
-                            that.options.dataContext.remove(that.model.csid);
-                            processChanges(that, false);
-                        }
-                    }
-                },
-                model: {
-                    messages: [ "recordEditor-dialog-deletePrimaryMessage" ]
-                },
-                termMap: [
-                    that.lookupMessage(that.options.recordType),
-                    that.hasMediaAttached(that) ? that.options.strings.deleteMessageMediaAttached : "",
-                    that.hasRelations(that) ? that.options.strings.deleteMessageWithRelated : ""
-                ],
-                parentBundle: that.options.parentBundle
-            });
+            cspace.recordEditor.remove(that);
         }
+    };
+    
+    cspace.recordEditor.remove = function (that) {
+        that.confirmation.open("cspace.confirmation.deleteDialog", undefined, {
+            listeners: {
+                onClose: function (userAction) {
+                    if (userAction === "act") {
+                        that.options.dataContext.remove(that.model.csid);
+                        processChanges(that, false);
+                    }
+                }
+            },
+            model: {
+                messages: [ "recordEditor-dialog-deletePrimaryMessage" ]
+            },
+            termMap: [
+                that.lookupMessage(that.options.recordType),
+                that.hasMediaAttached(that) ? that.options.strings.deleteMessageMediaAttached : "",
+                that.hasRelations(that) ? that.options.strings.deleteMessageWithRelated : ""
+            ],
+            parentBundle: that.options.parentBundle
+        });
     };
     
     cspace.recordEditor.rollback = function (that) {
