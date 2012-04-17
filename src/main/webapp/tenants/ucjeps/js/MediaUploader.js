7793a15b01c2a00007f40ad6e798607979249834
Ray Lee <rhlee@berkeley.edu>
2012-04-17 13:04:36 -0700

    UCJEPS-183: Copy in customized files from ucjeps 2.0. (cherry picked from commit 224b3f2500987a870d9fb71f4b119a44844bdbcc)

---
+++ src/main/webapp/tenants/ucjeps/js/MediaUploader.js
@@ -0,0 +1,314 @@
+/*
+Copyright 2011 Museum of Moving Image
+
+Licensed under the Educational Community License (ECL), Version 2.0. 
+You may not use this file except in compliance with this License.
+
+You may obtain a copy of the ECL 2.0 License at
+https://source.collectionspace.org/collection-space/LICENSE.txt
+*/
+
+/*global cspace:true, jQuery, fluid*/
+
+cspace = cspace || {};
+
+(function ($, fluid) {
+    "use strict";
+
+    cspace.mediaUploader = function (container, options) {
+        var that = fluid.initRendererComponent("cspace.mediaUploader", container, options);
+        fluid.initDependents(that);
+        that.refreshView();
+        that.bindEvents();
+        return that;
+    };
+    
+    // TODO: This is a hack to imitate focus on the file input and simulate a single file uploader. 
+    cspace.mediaUploader.bindEvents = function (that) {
+        if (!that.fileUploader.strategy) {
+            return;
+        }
+        var buttonView = that.fileUploader.strategy.local.browseButtonView;
+        if (!buttonView) {
+            return;
+        }
+        buttonView.container.delegate(buttonView.options.selectors.fileInputs, "focus", function () {
+            buttonView.locate("browseButton").addClass(that.options.styles.fileInputFocused);
+            var existing = that.fileUploader.queue.files[0];
+            if (existing) {
+                that.fileUploader.removeFile(existing);
+            }
+        });
+        buttonView.container.delegate(buttonView.options.selectors.fileInputs, "blur", function () {
+            buttonView.locate("browseButton").removeClass(that.options.styles.fileInputFocused);
+        });
+    };
+    
+    cspace.mediaUploader.refreshView = function (that) {
+        that.renderer.refreshView();
+    };
+    
+    cspace.mediaUploader.afterFileQueuedListener = function (input, file) {
+        input.text(file.name);
+    };
+    
+    cspace.mediaUploader.onFileSuccess = function (that, input, file, responseText, xhr) {
+        var response = JSON.parse(responseText);
+        that.applier.requestChange(that.options.elPaths.srcUri, response.file);
+        delete response.file;
+        that.applier.requestChange(that.options.elPaths.blobs, [response]);
+        that.applier.requestChange(that.options.elPaths.blobCsid, response.csid);
+        // TODO: When the onLink event listener triggers rerender and reinstantiation of media uploader this uploader dies :(.
+        setTimeout(function () {
+            that.events.onLink.fire();
+        }, 1);
+    };
+    
+    cspace.mediaUploader.onFileError = function (that, file, error, responseText, xhr) {
+        cspace.util.provideErrorCallback(that, that.options.urls.upload, "errorWriting")(error, responseText, xhr);
+        return false;
+    };;
+    
+    cspace.mediaUploader.produceTree = function (that) {
+        return {
+            expander: [{
+                type: "fluid.renderer.condition",
+                condition: "${" + that.options.elPaths.blobCsid + "}",
+                trueTree: {
+                    removeButton: {
+                        messagekey: "mediaUploader-removeButton",
+                        decorators: [{
+                            type: "addClass",
+                            classes: that.options.styles.removeButton
+                        }, {
+                            type: "jQuery",
+                            func: "click",
+                            args: that.removeMedia
+                        }]
+                    },
+                    uploader: {
+                        decorators: {
+                            type: "addClass",
+                            classes: that.options.styles.hidden
+                        }
+                    }
+                },
+                falseTree: {
+                    uploader: {},
+                    removeButton: {
+                        decorators: {
+                            type: "addClass",
+                            classes: that.options.styles.hidden
+                        }
+                    }
+                }
+            }],
+            uploadMediaLabel: {
+                messagekey: "mediaUploader-uploadMediaLabel"
+            },
+            linkMediaLabel: {
+                messagekey: "mediaUploader-linkMediaLabel"
+            },
+            uploadButton: {
+                messagekey: "mediaUploader-uploadButton",
+                decorators: [{
+                    type: "addClass",
+                    classes: that.options.styles.button
+                }]
+            },
+            linkInput: {
+                decorators: [{
+                    type: "jQuery",
+                    func: "keyup",
+                    args: that.processLink
+                }]
+            },
+            linkButton: {
+                messagekey: "mediaUploader-linkButton",
+                decorators: [{
+                    type: "addClass",
+                    classes: that.options.styles.button
+                }, {
+                    type: "jQuery",
+                    func: "click",
+                    args: that.linkMedia
+                }]
+            }
+        };
+    };
+    
+    cspace.mediaUploader.linkMedia = function (that) {
+        var srcUri = that.locate("linkInput").val();
+        that.options.applier.requestChange(that.options.elPaths.srcUri, srcUri);
+        that.events.onLink.fire();
+    };
+    
+    cspace.mediaUploader.processLink = function (that) {
+        that.locate("linkButton").prop("disabled", that.locate("linkInput").val() ? false : true);
+    };
+    
+    cspace.mediaUploader.assertBlob = function (blobCsid) {
+        return !!blobCsid;
+    };
+    
+    cspace.mediaUploader.removeMedia = function (that) {
+        that.confirmation.open("cspace.confirmation.deleteDialog", undefined, {
+            model: {
+                messages: ["mediaUploader-dialog-removePrimaryMessage", "mediaUploader-dialog-removeSecondaryMessage"],
+                messagekeys: {
+                    actText: "mediaUploader-dialog-removeActText",
+                    actAlt: "mediaUploader-dialog-removeActAlt"
+                }
+            },
+            listeners: {
+                onClose: function (userAction) {
+                    if (userAction === "act") {
+                        that.options.applier.requestChange(that.options.elPaths.blobCsid, "");
+                        that.events.onRemove.fire();
+                    }
+                }
+            },
+            parentBundle: that.options.parentBundle
+        });
+    };
+    
+    fluid.defaults("cspace.mediaUploader", {
+        gradeNames: "fluid.rendererComponent",
+        parentBundle: "{globalBundle}",
+        invokers: {
+            bindEvents: {
+                funcName: "cspace.mediaUploader.bindEvents",
+                args: "{mediaUploader}"
+            },
+            refreshView: {
+                funcName: "cspace.mediaUploader.refreshView",
+                args: "{mediaUploader}"
+            },
+            linkMedia: {
+                funcName: "cspace.mediaUploader.linkMedia",
+                args: "{mediaUploader}"
+            },
+            processLink: {
+                funcName: "cspace.mediaUploader.processLink",
+                args: "{mediaUploader}"
+            },
+            removeMedia: {
+                funcName: "cspace.mediaUploader.removeMedia",
+                args: "{mediaUploader}"
+            },
+            displayErrorMessage: "cspace.util.displayErrorMessage",
+            lookupMessage: "cspace.util.lookupMessage",
+            onFileError: {
+                funcName: "cspace.mediaUploader.onFileError",
+                args: ["{mediaUploader}", "{arguments}.0", "{arguments}.1", "{arguments}.2", "{arguments}.3"]
+            },
+            onFileSuccess: {
+                funcName: "cspace.mediaUploader.onFileSuccess",
+                args: ["{mediaUploader}", "{mediaUploader}.dom.uploadInput", "{arguments}.0", "{arguments}.1", "{arguments}.2"]
+            },
+            afterFileQueuedListener: {
+                funcName: "cspace.mediaUploader.afterFileQueuedListener",
+                args: ["{mediaUploader}.dom.uploadInput", "{arguments}.0"]
+            }
+        },
+        elPaths: {
+            blobCsid: "fields.blobCsid",
+            srcUri: "fields.srcUri",
+            blobs: "fields.blobs"
+        },
+        mergePolicy: {
+            model: "preserve",
+            applier: "nomerge"
+        },
+        selectors: {
+            uploadInput: ".csc-mediaUploader-uploadInput",
+            linkInput: ".csc-mediaUploader-linkInput",
+            uploadButton: ".csc-mediaUploader-uploadButton",
+            linkButton: ".csc-mediaUploader-linkButton",
+            removeButton: ".csc-mediaUploader-removeMedia",
+            fileUploader: ".csc-mediaUploader-fileUploaderContainer",
+            uploadMediaLabel: ".csc-mediaUploader-uploadMedia-label",
+            linkMediaLabel: ".csc-mediaUploader-linkMedia-label",
+            uploader: ".csc-mediaUploader"
+        },
+        selectorsToIgnore: ["fileUploader", "uploadInput"],
+        strings: {},
+        styles: {
+            button: "cs-mediaUploader-button",
+            hidden: "hidden",
+            removeButton: "cs-mediaUploader-removeMedia",
+            fileInputFocused: "cs-mediaUploader-fileInputFocused"
+        },
+        events: {
+            onLink: null,
+            onRemove: null
+        },
+        produceTree: cspace.mediaUploader.produceTree,
+        components: {
+            uploaderContext: {
+                type: "fluid.progressiveChecker",
+                priority: "first",
+                options: {
+                    checks: [{
+                        feature: "{fluid.browser.supportsBinaryXHR}",
+                        contextName: "fluid.uploader.html5"
+                    }, {
+                        feature: "{fluid.browser.supportsFlash}",
+                        contextName: "fluid.uploader.swfUpload"
+                    }],
+                    defaultContextName: "fluid.uploader.singleFile"
+                }
+            },
+            confirmation: "{confirmation}",
+            fileUploader: {
+                type: "fluid.uploader",
+                createOnEvent: "afterRender",
+                container: "{mediaUploader}.dom.fileUploader",
+                options: {
+                    components: {
+                        fileQueueView: {
+                            type: "fluid.emptySubcomponent"
+                        },
+                        totalProgressBar: {
+                            type: "fluid.emptySubcomponent"
+                        },
+                        errorPanel: {
+                            type: "fluid.emptySubcomponent"
+                        },
+                        strategy: {
+                            options: {
+                                flashMovieSettings: {
+                                    flashURL: "../lib/infusion/lib/swfupload/flash/swfupload.swf"
+                                }
+                            }
+                        }
+                    },
+                    queueSettings: {
+                        uploadURL: "{mediaUploader}.options.urls.upload",
+                        fileUploadLimit: 1,
+                        fileQueueLimit : 1
+                    },
+                    selectors: {
+                        uploadButton: "{mediaUploader}.options.selectors.uploadButton"
+                    }
+                }
+            }
+        },
+        resources: {
+            template: {
+                expander: {
+                    type: "fluid.deferredInvokeCall",
+                    func: "cspace.specBuilder",
+                    args: {
+                        forceCache: true,
+                        fetchClass: "fastTemplate",
+                        url: "%webapp/html/components/MediaUploaderTemplate.html"
+                    }
+                }
+            }
+        }
+    });
+    
+    fluid.fetchResources.primeCacheFromResources("cspace.mediaUploader");
+    
+})(jQuery, fluid);
---
+++ src/main/webapp/tenants/ucjeps/js/Sidebar.js
@@ -0,0 +1,402 @@
+/*
+Copyright 2010 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0. 
+You may not use this file except in compliance with this License.
+
+You may obtain a copy of the ECL 2.0 License at
+https://source.collectionspace.org/collection-space/LICENSE.txt
+*/
+
+/*global jQuery, cspace:true, fluid*/
+"use strict";
+
+cspace = cspace || {};
+
+(function ($, fluid) {
+    fluid.log("Sidebar.js loaded");
+    
+    fluid.registerNamespace("cspace.sidebar");
+    
+    var setupSideBar = function (that) {
+        that.locate("numOfTerms").text(fluid.stringTemplate(that.lookupMessage("sidebar-numOfTerms"), {
+            numOfTerms: that.termsUsed.calculateRecordListSize()
+        }));
+    }; 
+
+    cspace.sidebar = function (container, options) {
+        var that = fluid.initRendererComponent("cspace.sidebar", container, options);
+        restrictRelatedRecordLists(that);
+        fluid.initDependents(that);
+        that.renderer.refreshView();
+        
+        that.options.recordApplier.modelChanged.addListener("termsUsed", function (model, oldModel, changeRequest) {
+            that.termsUsed.applier.requestChange("items", model.termsUsed);
+            that.termsUsed.refreshView();
+            setupSideBar(that);
+        });
+        
+        setupSideBar(that);
+        
+        return that;
+    };
+    
+    /**
+     * Checks whether user has "list" permissions to the record types listed by the
+     * relatedRecordLists in this sidebar. If this is not the case, remove them from
+     * options.components.
+     * @param that the sidebar component
+     * @return modified that.options.component based on permissions
+     */
+    var restrictRelatedRecordLists = function (that) {
+        cspace.util.modelBuilder.fixupModel(that.model);
+        //TODO: alternatively have the relatedRecordListcomponents
+        // call this function before rendering, so we can avoid
+        // looking through each component:
+        var components = that.options.components;
+        fluid.remove_if(components, function (val, key) {
+            //search compoents for relatedRecordList components
+            if (val !== undefined && val.type === "cspace.relatedRecordsList") {
+                //check if we have list perms to record category defined by key
+                return cspace.permissions.getPermissibleRelatedRecords(key, that.options.resolver, that.options.recordTypeManager, "list").length === 0;
+            }
+        });
+    };
+    
+    cspace.sidebar.buildModel = function (options, records) {
+        if (!records || records.length < 1) {
+            return;
+        }
+        return {
+            "name": options.related,
+            categoryClass: "csc-related-" + options.related,
+            list: records
+        };
+    };
+
+    /**
+     * produce tree based on model. Create as many repeats of the
+     * categoryContainer field as dictated by the categories object in the model.
+     * For each repeat add a class as defined in the {categoryName}.categoryClass
+     */
+    cspace.sidebar.produceTree = function (that) {
+        return {
+            media: {
+                decorators: {
+                    type: "fluid",
+                    func: "cspace.sidebar.media"
+                }
+            },
+            termsHeader: {
+                messagekey: "sidebar-termsHeader"
+            },
+            expander: [{
+                repeatID: "categoryContainer",
+                type: "fluid.renderer.repeat",
+                pathAs: "category",
+                valueAs: "categoryName",
+                controlledBy: "categories",
+                tree: {
+                        decorators: [{
+                        type: "addClass",
+                        classes: "{categoryName}.categoryClass"
+                    }]
+                }
+            }]
+        };
+    };
+    
+    fluid.defaults("cspace.sidebar.media", {
+        gradeNames: ["fluid.rendererComponent", "autoInit"],
+        preInitFunction: "cspace.sidebar.media.preInit",
+        finalInitFunction: "cspace.sidebar.media.finalInit",
+        produceTree: "cspace.sidebar.media.produceTree",
+        invokers: {
+            showMediaImage: {
+                funcName: "cspace.sidebar.media.showMediaImage",
+                args: "{media}.model"
+            },
+            lookupMessage: {
+                funcName: "cspace.util.lookupMessage",
+                args: ["{sidebar}.options.parentBundle.messageBase", "{arguments}.0"]
+            }
+        },
+        components: {
+            togglable: {
+                type: "cspace.util.togglable",
+                createOnEvent: "afterRender",
+                options: {
+                    selectors: {
+                        header: "{media}.options.selectors.header",
+                        togglable: "{media}.options.selectors.togglable"
+                    }
+                }
+            }
+        },
+        selectors: {
+            mediaHeader: ".csc-sidebar-mediaHeader",
+            mediaSnapshot: ".csc-media-snapshot",
+            mediumImage: ".csc-sidebar-mediumImage",
+			imageLink: ".csc-sidebar-imageLink",
+            header: ".csc-media-header",
+            togglable: ".csc-media-togglable"
+        },
+        parentBundle: "{globalBundle}",
+        selectorsToIgnore: ["header", "togglable"],
+        styles: {
+            mediumImage: "cs-sidebar-mediumImage",
+            mediaSnapshot: "cs-media-snapshot-image"
+        },
+        strings: { }
+    });
+    
+    cspace.sidebar.media.showMediaImage = function (model) {
+        if (!model.fields) {
+            return false;
+        }
+
+		//added for the first related media record:
+		if (model.fields.blobCsid) {
+			return !!(model.fields.blobs && model.fields.blobs.length > 0);
+		} else if (model.relations) {
+			if (!model.relations.media) {
+				return false;
+			}
+			if (model.relations.media.length <= 0) {
+				return false;
+			}
+			return !!(model.relations.media[0].summarylist.imgThumb);
+		} else {
+			return false;
+		}
+    };
+    
+    cspace.sidebar.media.finalInit = function (that) {
+        that.refreshView();
+    };
+    
+    cspace.sidebar.media.preInit = function (that) {
+        that.getImageSource = function () {
+            var src = "";
+            if (!that.model.fields) {
+                return src;
+            }
+
+			if (that.model.fields.blobCsid) {
+				if (!that.model.fields.blobs) {
+					return src;
+				}
+				if (that.model.fields.blobs.length <= 0) {
+					return src;
+				}
+				return that.model.fields.blobs[0].imgMedium;
+			} else if (that.model.relations.media) {
+				if (that.model.relations.media.length <= 0) {
+					return src;
+				}
+				if (!that.model.relations.media[0].summarylist.imgThumb) {
+					return src;
+				}
+				var s = that.model.relations.media[0].summarylist.imgThumb;
+				s = s.replace(/Thumbnail$/, "Medium");
+				return s;
+			} else {
+				return src;
+			}			
+        };
+
+		that.getOriginalImage = function () {
+			if ((that.getImageSource() != "")) {
+				var src = ""
+				if (that.model.fields.blobCsid) {
+					src = that.model.fields.blobs[0].imgOrig;
+				} else if (that.model.relations.media) {
+					src = that.model.relations.media[0].summarylist.imgOrig;
+				} else {
+					return "";
+				}
+				
+				window.open(src, "_blank", fluid.stringTemplate(that.lookupMessage("media-originalMediaOptions"), {
+					height: "600",
+					width: "800",
+					scrollbars: "yes"
+				}));
+			}
+		};
+		
+        that.applier.modelChanged.addListener("fields.blobCsid", function () {
+            that.refreshView();
+        });
+
+    };
+    
+    cspace.sidebar.media.produceTree = function (that) {
+        return {
+            mediaHeader: {
+                messagekey: "sidebar-mediaHeader"
+            },
+            expander: {
+                type: "fluid.renderer.condition",
+                condition: that.showMediaImage(),
+                trueTree: {
+                    mediumImage: {
+                        decorators: [{
+                            addClass: "{styles}.mediumImage"
+                        }, {
+                            type: "attrs",
+                            attributes: {
+                                alt: that.lookupMessage("sidebar-mediumImage"),
+                                src: that.getImageSource()
+                            }
+                        }, {
+							type: "jQuery",
+							func: "click", 
+							args: that.getOriginalImage
+						}]
+                    },
+                    mediaSnapshot: {
+                        decorators: [{
+                            addClass: "{styles}.mediaSnapshot"
+                        }]
+                    }
+                },
+                falseTree: {
+                    mediaSnapshot: {}
+                }
+            }
+        };
+    };
+    
+    fluid.defaults("cspace.sidebar", {
+        gradeNames: "fluid.rendererComponent",
+        components: {
+            report: {
+                type: "cspace.reportProducer",
+                createOnEvent: "afterRender"
+            },
+            termsUsed: {
+                type: "cspace.recordList",
+                createOnEvent: "afterRender",
+                options: {
+                    model: {
+                        items: "{sidebar}.options.recordModel.termsUsed",
+                        messagekeys: {
+                            nothingYet: "sidebar-nothingYet"
+                        }
+                    },
+                    elPaths: {
+                        items: "items"
+                    },
+                    columns: ["number", "sourceFieldName", "recordtype"],
+                    strings: {
+                        number: "Term",
+                        sourceFieldName: "Field",
+                        recordtype: "Vocabulary"
+                    },
+                    showNumberOfItems: false
+                }
+            },
+            cataloging: {
+                type: "cspace.relatedRecordsList",
+                createOnEvent: "afterRender",
+                options: {
+                    primary: "{sidebar}.options.primaryRecordType",
+                    related: "cataloging",
+                    applier: "{sidebar}.options.recordApplier",
+                    model: "{sidebar}.options.recordModel",
+                    relationsElPath: "{sidebar}.options.relationsElPath"
+                }
+            },
+            procedures: {
+                type: "cspace.relatedRecordsList",
+                createOnEvent: "afterRender",
+                options: {
+                    primary: "{sidebar}.options.primaryRecordType",
+                    related: "procedures",
+                    applier: "{sidebar}.options.recordApplier",
+                    model: "{sidebar}.options.recordModel",
+                    relationsElPath: "{sidebar}.options.relationsElPath"
+                }
+            },
+            togglable: {
+                type: "cspace.util.togglable",
+                createOnEvent: "afterRender",
+                options: {
+                    selectors: {
+                        header: "{sidebar}.options.selectors.header",
+                        togglable: "{sidebar}.options.selectors.togglable"
+                    }
+                }
+            }
+        },
+        invokers: {
+            lookupMessage: {
+                funcName: "cspace.util.lookupMessage",
+                args: ["{sidebar}.options.parentBundle.messageBase", "{arguments}.0"]
+            }
+        },
+        model: {
+            categories: [{
+                expander: {
+                    type: "fluid.deferredInvokeCall",
+                    func: "cspace.util.modelBuilder",
+                    args: {
+                        callback: "cspace.sidebar.buildModel",
+                        related: "cataloging",
+                        resolver: "{permissionsResolver}",
+                        recordTypeManager: "{recordTypeManager}",
+                        permission: "list"
+                    }
+                }
+            }, {
+                expander: {
+                    type: "fluid.deferredInvokeCall",
+                    func: "cspace.util.modelBuilder",
+                    args: {
+                        callback: "cspace.sidebar.buildModel",
+                        related: "procedures",
+                        resolver: "{permissionsResolver}",
+                        recordTypeManager: "{recordTypeManager}",
+                        permission: "list"
+                    }
+                }
+            }]
+        },
+        produceTree: cspace.sidebar.produceTree,
+        mergePolicy: {
+            recordModel: "preserve",
+            recordApplier: "nomerge",
+            recordTypeManager: "nomerge",
+            resolver: "nomerge"
+        },
+        recordTypeManager: "{recordTypeManager}",
+        resolver: "{permissionsResolver}",
+        selectors: {
+            media: ".csc-sidebar-media",
+            numOfTerms: ".csc-num-items-terms",
+            termsUsed: ".csc-integrated-authorities",
+            "categoryContainer:": ".csc-related-record", //to be repeated
+            relatedCataloging: ".csc-related-cataloging",
+            relatedProcedures: ".csc-related-procedures",
+            termsHeader: ".csc-sidebar-termsHeader",
+            header: ".csc-sidebar-header",
+            togglable: ".csc-sidebar-togglable",
+            report: ".csc-sidebar-report"
+        },
+        parentBundle: "{globalBundle}",
+        selectorsToIgnore: ["report", "numOfTerms", "termsUsed", "relatedCataloging", "relatedProcedures", "header", "togglable"],
+        resources: {
+            template: cspace.resourceSpecExpander({
+                fetchClass: "fastTemplate",
+                url: "%webapp/html/components/SidebarTemplate.html",
+                options: {
+                    dataType: "html"
+                }
+            })
+        },
+        strings: { }
+    });
+        
+    fluid.fetchResources.primeCacheFromResources("cspace.sidebar");
+        
+})(jQuery, fluid);
