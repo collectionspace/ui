c096886c0701e5033dc2519b73d463c1392f3ad2
Ray Lee <rhlee@berkeley.edu>
2012-04-25 00:44:01 -0700

    UCJEPS-179: Initial check-in of computed field component.

---
+++ src/main/webapp/defaults/js/ComputedField.js
@@ -0,0 +1,179 @@
+/*
+Copyright 2011 Museum of Moving Image
+
+Licensed under the Educational Community License (ECL), Version 2.0. 
+You may not use this file except in compliance with this License.
+
+You may obtain a copy of the ECL 2.0 License at
+https://source.collectionspace.org/collection-space/LICENSE.txt
+ */
+
+/*global jQuery, fluid, cspace:true*/
+"use strict";
+
+cspace = cspace || {};
+
+(function ($, fluid) {
+    fluid.log("ComputedField.js loaded");
+    
+    fluid.defaults("cspace.computedField", {
+        gradeNames: ["fluid.viewComponent", "autoInit"],
+        preInitFunction: "cspace.computedField.preInit",
+        postInitFunction: "cspace.computedField.postInit",
+        finalInitFunction: "cspace.computedField.finalInit",
+        invokers: {
+            lookupMessage: "cspace.util.lookupMessage",
+            validate: {
+                funcName: "cspace.computedField.validate",
+                args: ["{computedField}", "{arguments}.0", "{messageBar}"]
+            },
+            clear: {
+                funcName: "cspace.computedField.clear",
+                args: "{messageBar}"
+            },
+            bindModelEvents: {
+                funcName: "cspace.computedField.bindModelEvents",
+                args: "{computedField}"
+            },
+            resolveElPath: {
+                funcName: "cspace.computedField.resolveElPath",
+                args: ["{computedField}", "{arguments}.0"]
+            },
+            calculateFieldValue: {
+                funcName: "cspace.computedField.calculateFieldValue",
+                args: "{computedField}"
+            },
+            getListenerNamespace: {
+                funcName: "cspace.computedField.getListenerNamespace",
+                args: ["{computedField}", "{arguments}.0"]
+            }
+        },
+        events: {
+            removeListeners: null
+        },
+        listeners: {
+            removeListeners: {
+                listener: "{computedField}.removeApplierListeners"
+            }
+        },
+
+        // The root EL path of this field in the model. This will be non-empty if this is a repeating field.
+        root: "",
+
+        // The EL path of this field, relative to the root.
+        elPath: "",
+
+        // The name of the calculation function used to compute the field value.
+        func: "cspace.computedField.joinArgs",
+
+        // Arguments to the calculation function. These are specified as EL paths.
+        // See cspace.computedField.bindModelEvents for details on how these paths are resolved in the model.
+        args: [],
+
+        // The datatype used for validation.
+        type: "string",
+
+        // The delay in ms to wait after a key is pressed before validating the field entry.
+        delay: 500
+    });
+       
+    cspace.computedField.preInit = function (that) {
+        that.removeApplierListeners = function () {
+            fluid.each(that.options.args, function(argElPath) {            
+                that.applier.modelChanged.removeListener(that.getListenerNamespace(argElPath));
+            });
+        };
+    };
+
+    cspace.computedField.postInit = function (that) {
+        that.container.keyup(function () {
+            clearTimeout(that.outFirer);
+            that.outFirer = setTimeout(function () {
+                that.clear();
+                var value = that.container.val();
+                that.validate(value);
+            }, that.options.delay);
+        });
+    };
+
+    cspace.computedField.finalInit = function (that) {
+        var label;
+        if (that.options.label) {
+            label = that.lookupMessage(that.options.label) + ": ";
+        };
+        that.invalidNumberMessage = fluid.stringTemplate(that.lookupMessage("invalidNumber"), {
+            label: label || ""
+        });
+
+        that.bindModelEvents();
+    };
+
+    /*
+     * Registers listeners for changes in the model. When the value at an EL path specified as an
+     * argument to the calculation function is changed, recompute the field value.
+     */
+    cspace.computedField.bindModelEvents = function (that) {
+        fluid.each(that.options.args, function(argElPath) {
+            var fullElPath = that.resolveElPath(argElPath);
+            
+            that.applier.modelChanged.addListener(fullElPath, function(model) {
+                var newValue = that.calculateFieldValue();
+                that.container.val(newValue);
+            }, that.getListenerNamespace(argElPath));
+        });
+    };
+
+    /*
+     * Calculates the field value, applying the configured calculation function to the values
+     * of the argument EL paths in the model.
+     */
+    cspace.computedField.calculateFieldValue = function (that) {
+        var args = [];
+
+        fluid.each(that.options.args, function(argElPath) {
+            var fullElPath = that.resolveElPath(argElPath);
+            args.push(fluid.get(that.model, fullElPath));
+        });
+
+        return fluid.invoke(that.options.func, args);
+    };
+
+    /*
+     * Resolve the given EL path.
+     * The argument elPath is simply appended to the root, so argument fields must be under the same root as this field.
+     * TODO: Make this function smarter about finding argument elPaths that are outside of this field's root.
+     * Returns the full EL path.
+     */
+    cspace.computedField.resolveElPath = function (that, elPath) {
+        return cspace.util.composeSegments(that.options.root, elPath);
+    };
+    
+    /*
+     * Returns the passed arguments as a comma separated string. This is useful as a default calculation function.
+     */
+    cspace.computedField.joinArgs = function () {
+        var args = Array.prototype.slice.call(arguments);
+        return args.join(", ");
+    };
+
+    /*
+     * Returns a unique namespace name for a given EL path.
+     */
+    cspace.computedField.getListenerNamespace = function (that, elPath) {
+        return  ("elPath-" + elPath + "-" + that.id);
+    };
+
+    cspace.computedField.validate = function (that, value, messageBar) {
+        var valid = true;
+        
+        if (that.options.type && that.options.type != "string") {
+            valid = cspace.util.validate(value, that.options.type, messageBar, that.invalidNumberMessage);            
+        }
+        
+        return valid;
+    }
+
+    cspace.computedField.clear = function (messageBar) {
+        messageBar.hide();
+    };
+})(jQuery, fluid);
