0296ff2cf8911efa802f40b1db8c8e7fee104644
Ray Lee <rhlee@berkeley.edu>
2012-06-21 22:42:52 -0700

    PAHMA-206: Added messages and styles, and only add relations for records on the current page.

--- src/main/webapp/defaults/js/SearchResultsRelationManager.js
+++ src/main/webapp/defaults/js/SearchResultsRelationManager.js
@@ -24,7 +24,12 @@ cspace = cspace || {};
 			dataContext: {
 				type: "cspace.dataContext",
 				options: {
-					recordType: "relationships"
+					recordType: "relationships",
+					listeners: {
+						onSave: "{loadingIndicator}.events.showOn.fire",
+						onError: "{loadingIndicator}.events.hideOn.fire",
+						afterSave: "{loadingIndicator}.events.hideOn.fire"
+					}
 				}
 			},
 			searchToRelateDialog: {
@@ -38,6 +43,10 @@ cspace = cspace || {};
 					relationsElPath: "relations",
 					listeners: {
 						addRelations: "{searchResultsRelationManager}.addRelations"
+					},
+					strings: {
+						title: "searchResultsRelationManager-dialogTitle",
+						addButton: "searchResultsRelationManager-addButton"
 					}
 				}
 			}
@@ -54,6 +63,18 @@ cspace = cspace || {};
 			addRelations: {
 				funcName: "cspace.searchResultsRelationManager.addRelations",
 				args: ["{searchResultsRelationManager}", "{arguments}.0", "{search}.model"] // FIXME: This should be in demands, since we don't know if we're in the context of search.
+			},
+			showMessage: {
+				funcName: "cspace.searchResultsRelationManager.showMessage",
+				args: ["{messageBar}", "{arguments}.0"]
+			},
+			showError: {
+				funcName: "cspace.searchResultsRelationManager.showError",
+				args: ["{messageBar}", "{arguments}.0"]
+			},
+			clearMessage: {
+				funcName: "cspace.searchResultsRelationManager.clearMessage",
+				args: "{messageBar}"
 			}
 		},
 		parentBundle: "{globalBundle}",
@@ -64,6 +85,14 @@ cspace = cspace || {};
 		that.events.onRelateButtonClick.addListener(function() {
 			that.add();
 		});
+		
+		that.dataContext.events.afterAddRelations.addListener(function(relations) {
+			afterAddRelations(that, relations);
+		});
+		
+		that.dataContext.events.onError.addListener(function(operation, message, data) {
+			onError(that, operation, message, data);
+		});        
     };
 	
 	cspace.searchResultsRelationManager = function(container, options) {
@@ -93,27 +122,79 @@ cspace = cspace || {};
 
 	cspace.searchResultsRelationManager.addRelations = function(that, dialogRelations, searchModel) {
 		/*
-		 * The searchToRelateDialog returns relations with the source/target swapped from what we want.
-		 * We also need to create a relation for each record in the result set.
+		 * The searchToRelateDialog returns relations with the source/target swapped from what we want (although I'm not sure it matters, since the relation isn't one-way).
+		 * We also need to create a relation for each record in the result set (on the current page).
 		 */
-		var transformedItems = [];
 		var results = searchModel.results;
+
+		if (dialogRelations.items.length > 0 && results.length > 0) {
+			var transformedItems = [];
+			var start = searchModel.offset;
+			var end = Math.min(start + parseInt(searchModel.pagination.pageSize), parseInt(searchModel.pagination.totalItems));
 		
-		for (var i=0; i<dialogRelations.items.length; i++) {
-			var dialogRelation = dialogRelations.items[i];
+			for (var i=0; i<dialogRelations.items.length; i++) {
+				var dialogRelation = dialogRelations.items[i];
 			
-			for (var j in results) {
-				transformedItems.push({
-					source: dialogRelation.target,
-					target: results[j],
-					"one-way": dialogRelation["one-way"],
-					type: dialogRelation.type
-				});
+				for (var j=start; j<end; j++) {
+					transformedItems.push({
+						source: dialogRelation.target,
+						target: results[j],
+						"one-way": dialogRelation["one-way"],
+						type: dialogRelation.type
+					});
+				}
 			}
+
+			that.dataContext.addRelations({
+				items: transformedItems
+			});
+		}
+	};
+	
+	var afterAddRelations = function(that, relations) {
+		var items = relations.items;
+		var sources = {};
+		var counts = {};
+		
+		for (var i=0; i<items.length; i++) {
+			var item = items[i];
+			var source= item.source;
+			var sourceCsid = source.csid;
+			
+			if (!sources[sourceCsid]) {
+				sources[sourceCsid] = source;
+			}
+						
+			var count = (sourceCsid in counts) ? counts[sourceCsid] : 0;
+			counts[sourceCsid] = count + 1;
 		}
 		
-		that.dataContext.addRelations({
-			items: transformedItems
-		});
+		var sourceNumbers = [];
+		var count = 0;
+		
+		for (var csid in sources) {
+			sourceNumbers.push(sources[csid].number);
+			count = counts[csid];
+		}
+		
+		sourceNumbers = sourceNumbers.sort();
+		
+		that.showMessage(count + " " + (count == 1 ? "record" : "records") + " added to " + sourceNumbers.join(", ")); // FIXME: Move to message bundle
+	}
+	
+	var onError = function(that, operation, message, data) {
+		that.showError("Error: " + message + ((typeof(data) != "undefined") ? (": " + data) : ""));
+	}
+	
+	cspace.searchResultsRelationManager.showMessage = function (messageBar, message) {
+		messageBar.show(message, null, false);
+	}
+	
+	cspace.searchResultsRelationManager.showError = function (messageBar, message) {
+		messageBar.show(message, null, true);
+	}
+
+	cspace.searchResultsRelationManager.clearMessage = function (messageBar) {
+		messageBar.hide();
 	};
 })(jQuery, fluid);
