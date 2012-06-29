2b0e096b5b9fa1a4b3901c351f497ccc6b000646
Yura Zenevich <yura.zenevich@gmail.com>
2012-06-29 10:32:23 -0400

    CSPACE-5254: Fixing issue with page size changes

--- src/main/webapp/defaults/js/ListView.js
+++ src/main/webapp/defaults/js/ListView.js
@@ -494,6 +494,7 @@ cspace = cspace || {};
         var rows = that.locate("row");
         fluid.each(rows, function (row, index) {
             var record = that.options.list[that.options.offset + index];
+            if (!record) {return;}
             that.locate("column", rows.eq(index)).wrapInner($("<a/>").attr("href", fluid.stringTemplate(that.options.url, {
                 recordType: record[that.options.typePath].toLowerCase(),
                 csid: record.csid
