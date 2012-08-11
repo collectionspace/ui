61e34a0c7fca0ab9af59ea1ad69065b64106ab86
Ray Lee <rhlee@berkeley.edu>
2012-08-10 17:35:43 -0700

    PAHMA-206: Hide Add to Record button when search is for an authority item.

--- src/main/webapp/defaults/js/RelateSearchResults.js
+++ src/main/webapp/defaults/js/RelateSearchResults.js
@@ -1,12 +1,28 @@
-/*
-Copyright 2010 University of California, Berkeley
+/**
+ *  This document is a part of the source code and related artifacts
+ *  for CollectionSpace, an open source collections management system
+ *  for museums and related institutions:
 
-Licensed under the Educational Community License (ECL), Version 2.0. 
-You may not use this file except in compliance with this License.
+ *  http://www.collectionspace.org
+ *  http://wiki.collectionspace.org
 
-You may obtain a copy of the ECL 2.0 License at
-https://source.collectionspace.org/collection-space/LICENSE.txt
-*/
+ *  Copyright 2009 University of California at Berkeley
+
+ *  Licensed under the Educational Community License (ECL), Version 2.0.
+ *  You may not use this file except in compliance with this License.
+
+ *  You may obtain a copy of the ECL 2.0 License at
+
+ *  https://source.collectionspace.org/collection-space/LICENSE.txt
+
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ *  $LastChangedRevision$
+ */
 
 /*global jQuery, cspace:true, fluid*/
 
@@ -24,12 +40,16 @@ cspace = cspace || {};
 		},
 		events: {
 			onRelateButtonClick: null,
-			recordTypeChanged: null
+			recordTypeChanged: "{search}.mainSearch.events.recordTypeChanged"
 		},
 		invokers: {
 			recordTypeChanged: {
 				funcName: "cspace.relateSearchResults.recordTypeChanged",
-				args: ["{relateSearchResults}", "{arguments}.0", "{advancedSearch}.vocabSelector.model"]
+				args: ["{relateSearchResults}", "{arguments}.0", "{search}.mainSearch.recordTypeSelector.model"]
+			},
+			showRelateButton: {
+				funcName: "cspace.relateSearchResults.showRelateButton",
+				args: ["{relateSearchResults}", "{arguments}.0"]
 			}
 		},
 		parentBundle: "{globalBundle}",
@@ -41,13 +61,13 @@ cspace = cspace || {};
 		that.handleRecordTypeChanged = function(recordType) {
 			that.recordTypeChanged(recordType);
 		}
-	}
+	};
 	
 	cspace.relateSearchResults.finalInit = function(that) {
 		that.events.recordTypeChanged.addListener(that.handleRecordTypeChanged);
 		
 		that.refreshView();
-	}
+	};
 
 	cspace.relateSearchResults.produceTree = function(that) {
 		return {
@@ -68,7 +88,18 @@ cspace = cspace || {};
 		};
 	};
 	
-	cspace.relateSearchResults.recordTypeChanged = function(that, recordType, vocabSelectorModel) {
-		var foo = "";
+	cspace.relateSearchResults.recordTypeChanged = function(that, recordType, recordTypes) {
+		var isVocab = $.inArray(recordType, recordTypes.vocabularies) >= 0;
+		
+		if (isVocab) {
+			that.showRelateButton(false);
+		}
+		else {
+			that.showRelateButton(true);
+		}
+	};
+	
+	cspace.relateSearchResults.showRelateButton = function(that, show) {
+		that.locate("relateButton").toggleClass("hidden", !show);
 	}
 })(jQuery, fluid);
