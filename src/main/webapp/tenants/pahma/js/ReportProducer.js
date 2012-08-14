4bef3024e1f165e5a31bf64b9f7db2f1253edaf3
Ray Lee <rhlee@berkeley.edu>
2012-08-13 18:59:50 -0700

    PAHMA-344: Make reports open in a new window.

--- src/main/webapp/tenants/pahma/js/ReportProducer.js
+++ src/main/webapp/tenants/pahma/js/ReportProducer.js
@@ -236,7 +236,7 @@ cspace = cspace || {};
                             });
                             return;
                         }
-                        window.location = href;
+                        window.open(href, "_blank");
                         events.reportFinished.fire();
                         if (callback) {
                             callback();
