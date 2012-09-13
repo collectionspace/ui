339f16563cd4db7cfc5a612f6034dcab23d8e55e
Yura Zenevich <yura.zenevich@gmail.com>
2012-09-13 15:45:04 -0400

    CSPACE-5486: Fixed the issue with request change event when the field did not actually change.

--- src/main/webapp/defaults/js/Autocomplete.js
+++ src/main/webapp/defaults/js/Autocomplete.js
@@ -754,8 +754,10 @@ https://source.collectionspace.org/collection-space/LICENSE.txt
             that.autocompleteInput.val() === termRecord.displayName) {
             return;
         }
-        that.hiddenInput.val(termRecord.urn);
-        that.hiddenInput.change();
+        if (that.hiddenInput.val() !== termRecord.urn) {
+            that.hiddenInput.val(termRecord.urn);
+            that.hiddenInput.change();
+        }
         fluid.log("New value " + termRecord.displayName);
         that.autocompleteInput.val(termRecord.displayName);
         that.applier.requestChange("baseRecord", fluid.copy(termRecord));
