a56a14c60dcef4951a850f8980e014db841a6307
Yura Zenevich <yura.zenevich@gmail.com>
2012-09-17 10:22:26 -0400

    NOJIRA: Updating version to 2.7

--- src/main/webapp/defaults/js/Footer.js
+++ src/main/webapp/defaults/js/Footer.js
@@ -66,8 +66,8 @@ cspace = cspace || {};
         model: {
             about: "http://www.collectionspace.org",
             currentRelease: "http://www.collectionspace.org/current_release",
-            feedback: "http://wiki.collectionspace.org/display/collectionspace/Release+2.6+Feedback",
-            version: "2.6"
+            feedback: "http://wiki.collectionspace.org/display/collectionspace/Release+2.7+Feedback",
+            version: "2.7"
         },
         resources: {
             template: {
