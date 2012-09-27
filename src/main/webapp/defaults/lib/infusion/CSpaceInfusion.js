2e6b4e3647f7a664772dcf8bccfe30b364a21449
Yura Zenevich <yura.zenevich@gmail.com>
2012-09-27 10:25:04 -0400

    CSPACE-2485: Removed un-caching of static resources.

--- src/main/webapp/defaults/lib/infusion/CSpaceInfusion.js
+++ src/main/webapp/defaults/lib/infusion/CSpaceInfusion.js
@@ -15983,7 +15983,7 @@ var fluid_1_5 = fluid_1_5 || {};
                  }
                  pendingClass[fetchClass][canon] = resourceSpec;
              }
-             options.cache = false; // TODO: Getting weird "not modified" issues on Firefox
+             // options.cache = false; // TODO: Getting weird "not modified" issues on Firefox
              $.ajax(options);
          }
          else {
