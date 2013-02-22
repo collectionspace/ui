e411e1702636bcd27a85c79743f145dd2c9ff900
Ray Lee <rhlee@berkeley.edu>
2013-02-22 12:57:54 -0800

    PAHMA-735,PAHMA-475,PAHMA-445: Fix search in searchToRelateDialog opening a new page.

--- src/main/webapp/defaults/js/Demands.js
+++ src/main/webapp/defaults/js/Demands.js
@@ -1522,10 +1522,10 @@ https://source.collectionspace.org/collection-space/LICENSE.txt
             funcName: "cspace.searchBox.navigateToSearch",
             args: ["{searchBox}", "{recordEditor}"]
         });
-        // fluid.demands("cspace.searchBox.navigateToSearch", ["cspace.searchBox", "cspace.search.searchView"], {
-        //     funcName: "cspace.search.handleSubmitSearch",
-        //     args: ["{searchBox}", "{searchView}"]
-        // });
+        fluid.demands("cspace.searchBox.navigateToSearch", ["cspace.searchBox", "cspace.search.searchView", "cspace.searchToRelateDialog"], {
+            funcName: "cspace.search.handleSubmitSearch",
+            args: ["{searchBox}", "{searchView}"]
+        });
         
         fluid.demands("cspace.termList.termListSource", ["cspace.termList"], {
             funcName: "cspace.URLDataSource",
