a765ed5c7b2d7bdfcd74bb21292d98a0f9575d9a
Aron Roberts <aron@socrates.berkeley.edu>
2013-07-01 13:11:51 -0700

    CSPACE-6067: Update UI layer text to reflect version numbering change to 3.3, from 3.2.2.

--- src/main/webapp/defaults/js/Footer.js
+++ src/main/webapp/defaults/js/Footer.js
@@ -69,8 +69,8 @@ cspace = cspace || {};
         model: {
             about: "http://www.collectionspace.org",
             currentRelease: "http://www.collectionspace.org/current_release",
-            feedback: "http://wiki.collectionspace.org/display/collectionspace/Release+3.2.3+Feedback",
-            version: "3.2.3"
+            feedback: "http://wiki.collectionspace.org/display/collectionspace/Release+3.3+Feedback",
+            version: "3.3"
         },
         // Footer template resource.
         resources: {
