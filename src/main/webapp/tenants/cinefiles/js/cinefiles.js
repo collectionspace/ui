94e072d9317e31a96e6de2c174e5186d011be9d8
Ray Lee <rhlee@berkeley.edu>
2013-10-09 12:35:38 -0700

    BAMPFA-32: Customize Work record editor form.

---
+++ src/main/webapp/tenants/cinefiles/js/cinefiles.js
@@ -0,0 +1,27 @@
+/*
+Copyright 2013 University of California, Berkeley
+
+Licensed under the Educational Community License (ECL), Version 2.0. 
+You may not use this file except in compliance with this License.
+
+You may obtain a copy of the ECL 2.0 License at
+https://source.collectionspace.org/collection-space/LICENSE.txt
+*/
+
+var cinefiles = {};
+
+(function ($, fluid) {
+	
+	cinefiles.computeWorkDisplayName = function(article, title) {
+		if (typeof(article) == "undefined") {
+			article = "";
+		}
+		
+		if (typeof(title) == "undefined") {
+			title = "";
+		}
+		
+		return article + title;
+	}
+	
+})(jQuery, fluid);
