6f0a2c163becdddb5364bc8f23016212f6f58380
Ray Lee <rhlee@berkeley.edu>
2014-03-03 18:04:27 -0800

    BAMPFA-137,BAMPFA-138: Add tests.

---
+++ src/test/java/edu/berkeley/cspace/it/cinefiles/CineFilesCatalogingIT.java
@@ -0,0 +1,42 @@
+package edu.berkeley.cspace.it.cinefiles;
+
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import edu.berkeley.cspace.driver.CollectionSpacePage;
+import edu.berkeley.cspace.driver.SaveFailedException;
+import edu.berkeley.cspace.it.CatalogingIT;
+
+public class CineFilesCatalogingIT extends CatalogingIT {
+
+	/**
+	 * Tests CineFiles customizations to the Cataloging form.
+	 * <ul>
+	 * <li>Identification Number should have CineFiles Document Number and PFA Stills Number patterns</li>
+	 * <li>Collection should be a dynamic term list, and should have CineFiles and PFA Stills options</li>
+	 * </ul>
+	 * @throws SaveFailedException 
+	 */
+	@Test(dependsOnMethods = { "testLogin" })
+	public void testFormCustomizations() {
+		driver.navigateTo(CollectionSpacePage.CATALOGING);
+		
+		// Check Identification Number patterns.
+		
+		List<String> patternNames = driver.getNumberPatternNames("csc-object-identification-object-number");
+		Assert.assertEquals(patternNames.size(), 2, "Incorrect number of Identification Number patterns:");
+		Assert.assertEquals(patternNames.get(0), "CineFiles Document Number", "Incorrect first pattern for Identification Number");
+		Assert.assertEquals(patternNames.get(1), "PFA Stills Number", "Incorrect second pattern for Identification Number");
+		
+		// Check options in the Collection term list.
+		
+		List<String> collectionOptions = driver.getOptionDataValues("csc-object-identification-collection");
+		
+		Assert.assertEquals(collectionOptions.size(), 3, "Incorrect number of Collection options:");
+		Assert.assertEquals(collectionOptions.get(0), "", "Incorrect first option for Collection:");
+		Assert.assertEquals(collectionOptions.get(1), "urn:cspace:cinefiles.cspace.berkeley.edu:vocabularies:name(collection):item:name(cinefiles)'CineFiles'", "Incorrect second option for Collection:");
+		Assert.assertEquals(collectionOptions.get(2), "urn:cspace:cinefiles.cspace.berkeley.edu:vocabularies:name(collection):item:name(pfastills)'PFA Stills'", "Incorrect third option for Collection:");
+	}
+}
