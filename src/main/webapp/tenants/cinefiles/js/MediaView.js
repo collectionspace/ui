a5e7b72befb30c971ee0f653767262d671f12b93
Ray Lee <rhlee@berkeley.edu>
2014-03-24 19:05:56 -0700

    BAMPFA-111: Check mime type when media snapshot and thumbnail are clicked.

--- src/main/webapp/tenants/cinefiles/js/MediaView.js
+++ src/main/webapp/tenants/cinefiles/js/MediaView.js
@@ -223,7 +223,10 @@ cspace = cspace || {};
             if (!url) {
                 return url;
             }
-            return url.replace(/Thumbnail/, format || "OriginalJpeg");
+            if (!format) {
+               format = (typeof(mimeType) == "undefined" || mimeType.indexOf("image/") == 0) ? "OriginalJpeg" : "Original";
+            }
+            return url.replace(/Thumbnail/, format);
         };
 
         // Function to return if record has the primary media
