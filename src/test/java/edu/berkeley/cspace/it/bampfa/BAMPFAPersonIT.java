a68233ef9c7ee2ddd631ef2be8e79d5bed9f0d49
Ray Lee <rhlee@berkeley.edu>
2014-05-08 14:41:38 -0700

    BAMPFA-183: Add person datesActive field.

---
+++ src/test/java/edu/berkeley/cspace/it/bampfa/BAMPFAPersonIT.java
@@ -0,0 +1,32 @@
+package edu.berkeley.cspace.it.bampfa;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import edu.berkeley.cspace.driver.CollectionSpacePage;
+import edu.berkeley.cspace.it.CollectionSpaceIT;
+
+public class BAMPFAPersonIT extends CollectionSpaceIT {
+	/**
+	 * Tests BAM/PFA customizations to the Person form.
+	 * <ul>
+	 * <li>The dates active field should appear (BAMPFA-183)</li>
+	 * </ul>
+	 */
+	@Test(dependsOnMethods = { "testLogin" })
+	public void testFormCustomizations() {
+		driver.navigateTo(CollectionSpacePage.PERSON);
+		driver.expandAllSections();
+
+		List<WebElement> elements;
+
+		// The dates active field should appear (BAMPFA-183)
+		
+		elements = driver.findElementsImmediately(By.className("csc-person-datesActive"));
+		Assert.assertEquals(elements.size(), 1, "the dates active field should be found:");
+	}
+}
