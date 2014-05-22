e7d76f5fc93631e668475567231a34692134aeac
Ray Lee <rhlee@berkeley.edu>
2014-05-21 19:05:32 -0700

    BAMPFA-190: Customize acquisition record.

--- src/test/java/edu/berkeley/cspace/it/bampfa/BAMPFAAcquisitionIT.java
+++ src/test/java/edu/berkeley/cspace/it/bampfa/BAMPFAAcquisitionIT.java
@@ -16,6 +16,13 @@ import edu.berkeley.cspace.it.CollectionSpaceIT;
  * Tests BAM/PFA customizations to the Acquisition form.
  * <ul>
  * <li>The acquisition method field should be a dynamic term list (BAMPFA-189)</li>
+ * <li>The legal status field should appear (BAMPFA-190)</li>
+ * <li>The status date field should appear (BAMPFA-190)</li>
+ * <li>The for field should appear (BAMPFA-190)</li>
+ * <li>The appraisal info field should appear (BAMPFA-190)</li>
+ * <li>The current value fields should appear (BAMPFA-190)</li>
+ * <li>The initial value fields should appear (BAMPFA-190)</li>
+ * <li>The acquisition source field should not be an autocomplete field (BAMPFA-190)</li>
  * </ul>
  */
 public class BAMPFAAcquisitionIT extends CollectionSpaceIT {
@@ -51,5 +58,54 @@ public class BAMPFAAcquisitionIT extends CollectionSpaceIT {
 		
 		Assert.assertTrue(value.startsWith("urn:cspace:bampfa.cspace.berkeley.edu:vocabularies:name(acquisitionmethod):item:name"), "the acquisition method field should be tied to the acquisitionmethod vocabulary:");
 
+		// The legal status field should appear (BAMPFA-190)
+		
+		elements = driver.findElementsImmediately(By.className("csc-acquisition-legalStatus"));
+		Assert.assertEquals(elements.size(), 1, "the legal status field should be found:");
+		
+		// The status date field should appear (BAMPFA-190)
+
+		elements = driver.findElementsImmediately(By.className("csc-acquisition-legalStatusDateGroup"));
+		Assert.assertEquals(elements.size(), 1, "the status date field should be found:");
+		
+		// The for field should appear (BAMPFA-190)
+		
+		elements = driver.findElementsImmediately(By.className("csc-acquisition-bampfaAcquisitionReason"));
+		Assert.assertEquals(elements.size(), 1, "the for field should be found:");
+		
+		// The appraisal info field should appear (BAMPFA-190)
+		
+		elements = driver.findElementsImmediately(By.className("csc-acquisition-appraisalInfo"));
+		Assert.assertEquals(elements.size(), 1, "the appraisal info field should be found:");
+		
+		// The current value fields should appear (BAMPFA-190)
+		
+		elements = driver.findElementsImmediately(By.className("csc-acquisition-currentValue"));
+		Assert.assertEquals(elements.size(), 1, "the current value field should be found:");
+
+		elements = driver.findElementsImmediately(By.className("csc-acquisition-currentValueSource"));
+		Assert.assertEquals(elements.size(), 1, "the current value source field should be found:");
+
+		elements = driver.findElementsImmediately(By.className("csc-currentValueGroup-currentValueDateGroup"));
+		Assert.assertEquals(elements.size(), 1, "the current value date field should be found:");
+
+		// The initial value fields should appear (BAMPFA-190)
+		
+		elements = driver.findElementsImmediately(By.className("csc-acquisition-initialValue"));
+		Assert.assertEquals(elements.size(), 1, "the initial value field should be found:");
+
+		elements = driver.findElementsImmediately(By.className("csc-acquisition-initialValueSource"));
+		Assert.assertEquals(elements.size(), 1, "the initial value source field should be found:");
+
+		elements = driver.findElementsImmediately(By.className("csc-initialValueGroup-initialValueDateGroup"));
+		Assert.assertEquals(elements.size(), 1, "the initial value date field should be found:");
+
+		// The acquisition source field should not be an autocomplete field (BAMPFA-190)
+		
+		elements = driver.findElementsImmediately(By.className("csc-acquisition-acquisitionSource"));
+		Assert.assertEquals(elements.size(), 1, "the acquisition source field should be found:");
+		
+		WebElement sourceField = elements.get(0);
+		Assert.assertTrue(!driver.isAutocomplete(sourceField), "the acquisition source field should not be an autocomplete field");
 	}
 }
