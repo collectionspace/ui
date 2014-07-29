7dc19320b3b7fc4e84e1adcb8ca15aece36c3b4e
Ray Lee <rhlee@berkeley.edu>
2014-07-28 17:02:29 -0700

    BAMPFA-205: Add LFM and FML name computation functions.

--- src/main/webapp/tenants/bampfa/js/bampfa.js
+++ src/main/webapp/tenants/bampfa/js/bampfa.js
@@ -79,6 +79,126 @@ var bampfa = {};
 		
 		return summaryParts.join(" ");
 	}
+
+	bampfa.computeFullNameLFM = function(foreName, middleName, surName, nameAdditions, nationalities) {
+		var nameParts = [];
+
+		foreName = jQuery.trim(foreName);
+		middleName = jQuery.trim(middleName);
+		surName = jQuery.trim(surName);
+		nameAdditions = jQuery.trim(nameAdditions);
+
+		var nationality = "";
+		
+		if (nationalities.length > 0) {
+			nationality = jQuery.trim(nationalities[0].nationality);
+		} 
+	
+		if (surName.toLowerCase() == "unknown") {
+			nameParts.push(surName);
+			
+			if (nationality) {
+				nameParts.push("(" + nationality + ")");
+			}
+		}
+		else if (!foreName && !surName) {
+			// The calculation function from FileMaker outputs "??" if both first name and last name are empty.
+			// As a simplification, I'm making the output empty in this case. See BAMPFA-205 and BAMPFA-238
+			// for details.
+			
+			// nameParts.push("??");
+		}
+		else if (!surName) {
+			nameParts.push(foreName);
+		}
+		else if (!foreName) {
+			nameParts.push(surName);
+		}
+		else if (!middleName) {
+			if (nationality.toLowerCase().indexOf("china") < 0) {
+				surName = surName + ",";
+			}
+			
+			nameParts.push(surName);
+			
+			if (nameAdditions) {
+				foreName = foreName + ",";
+			}
+			
+			nameParts.push(foreName);
+			
+			if (nameAdditions) {
+				nameParts.push(nameAdditions);
+			}
+		}
+		else {
+			if (nationality.toLowerCase().indexOf("china") < 0) {
+				surName = surName + ",";
+			}
+			
+			nameParts.push(surName);
+			nameParts.push(foreName);
+			
+			if (nameAdditions) {
+				middleName = middleName + ",";
+			}
+			
+			nameParts.push(middleName);
+			
+			if (nameAdditions) {
+				nameParts.push(nameAdditions);
+			}
+		}
+		
+		return nameParts.join(" ");
+	}
+	
+	bampfa.computeFullNameFML = function(title, foreName, middleName, surName, nameAdditions, nationalities) {
+		var nameParts = [];
+		
+		title = jQuery.trim(title);
+		foreName = jQuery.trim(foreName);
+		middleName = jQuery.trim(middleName);
+		surName = jQuery.trim(surName);
+		nameAdditions = jQuery.trim(nameAdditions);
+
+		var nationality = "";
+		
+		if (nationalities.length > 0) {
+			nationality = jQuery.trim(nationalities[0].nationality);
+		} 
+		
+		if (surName.toLowerCase() == "unknown") {
+			nameParts.push(surName);
+
+			if (nationality) {
+				nameParts.push("(" + nationality + ")");
+			}
+		}
+		else {
+			if (title) {
+				nameParts.push(title);
+			}
+			
+			if (foreName) {
+				nameParts.push(foreName);
+			}
+			
+			if (middleName) {
+				nameParts.push(middleName);
+			}
+			
+			if (surName) {
+				nameParts.push(surName);
+			}
+			
+			if (nameAdditions) {
+				nameParts.push(nameAdditions);
+			}			
+		}
+		
+		return nameParts.join(" ");
+	}
 	
 	/* 
 	 * Convert html from the richTextEditor component into plain text.
