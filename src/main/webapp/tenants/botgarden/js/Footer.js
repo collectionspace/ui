8a89a546868c31e02d45fb9ba9804afe60578eb1
Ray Lee <rhlee@berkeley.edu>
2014-08-22 22:22:49 -0700

    NOJIRA: (make-release) Update version numbers to botgarden_4.0-62 and generate release notes.

--- src/main/webapp/tenants/botgarden/js/Footer.js
+++ src/main/webapp/tenants/botgarden/js/Footer.js
@@ -87,7 +87,7 @@ cspace = cspace || {};
             deploymentRelease: "releaseNotes.html",
             feedback: "http://wiki.collectionspace.org/display/collectionspace/Release+4.0+Feedback",
             version: "4.0",
-            deploymentVersion: "botgarden_4.0-61",
+            deploymentVersion: "botgarden_4.0-62",
             support: "mailto:cspace-support@lists.berkeley.edu"
         },
         // Footer template resource.
