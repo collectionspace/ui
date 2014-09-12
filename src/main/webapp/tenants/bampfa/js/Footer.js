adb7e3965f65de07d3a4e1d0602bad88883988c1
Ray Lee <rhlee@berkeley.edu>
2014-09-11 23:55:14 -0700

    NOJIRA: (make-release) Update version numbers to bampfa_4.0-30 and generate release notes.

--- src/main/webapp/tenants/bampfa/js/Footer.js
+++ src/main/webapp/tenants/bampfa/js/Footer.js
@@ -87,7 +87,7 @@ cspace = cspace || {};
             deploymentRelease: "releaseNotes.html",
             feedback: "http://wiki.collectionspace.org/display/collectionspace/Release+4.0+Feedback",
             version: "4.0",
-            deploymentVersion: "bampfa_4.0-29",
+            deploymentVersion: "bampfa_4.0-30",
             support: "mailto:cspace-support@lists.berkeley.edu"
         },
         // Footer template resource.
