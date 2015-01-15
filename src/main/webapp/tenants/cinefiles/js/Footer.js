af2d2f1156807fc4ceb12d03efa7ad2e205ae45a
Ray Lee <rhlee@berkeley.edu>
2015-01-14 22:35:16 -0800

    NOJIRA: (make-release) Update version numbers to cinefiles_4.1-2 and generate release notes.

--- src/main/webapp/tenants/cinefiles/js/Footer.js
+++ src/main/webapp/tenants/cinefiles/js/Footer.js
@@ -87,7 +87,7 @@ cspace = cspace || {};
             deploymentRelease: "releaseNotes.html",
             feedback: "http://wiki.collectionspace.org/display/collectionspace/Release+4.1+Feedback",
             version: "4.1",
-            deploymentVersion: "cinefiles_4.1-1",
+            deploymentVersion: "cinefiles_4.1-2",
             support: "mailto:cspace-support@lists.berkeley.edu"
         },
         // Footer template resource.
