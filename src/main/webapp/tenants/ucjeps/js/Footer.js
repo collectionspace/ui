5243b2472828698643955ed4dbf02c6b464159d8
Ray Lee <rhlee@berkeley.edu>
2015-02-04 13:17:37 -0800

    NOJIRA: (make-release) Update version numbers to ucjeps_4.1-9 and generate release notes.

--- src/main/webapp/tenants/ucjeps/js/Footer.js
+++ src/main/webapp/tenants/ucjeps/js/Footer.js
@@ -87,7 +87,7 @@ cspace = cspace || {};
             deploymentRelease: "releaseNotes.html",
             feedback: "http://wiki.collectionspace.org/display/collectionspace/Release+4.1+Feedback",
             version: "4.1",
-            deploymentVersion: "ucjeps_4.1-8",
+            deploymentVersion: "ucjeps_4.1-9",
             support: "mailto:cspace-support@lists.berkeley.edu"
         },
         // Footer template resource.
