fa45c773c3b5539840e25ad47a9f6c34b56fd4ce
Ray Lee <rhlee@berkeley.edu>
2015-02-19 16:58:44 -0800

    NOJIRA: (make-release) Update version numbers to pahma_4.1-8 and generate release notes.

--- src/main/webapp/tenants/pahma/js/Footer.js
+++ src/main/webapp/tenants/pahma/js/Footer.js
@@ -87,7 +87,7 @@ cspace = cspace || {};
             deploymentRelease: "releaseNotes.html",
             feedback: "http://wiki.collectionspace.org/display/collectionspace/Release+4.1+Feedback",
             version: "4.1",
-            deploymentVersion: "pahma_4.1-7",
+            deploymentVersion: "pahma_4.1-8",
             support: "mailto:cspace-support@lists.berkeley.edu?subject=%5BPAHMA-CSpace%5D%20"
         },
         // Footer template resource.
