88542f5c18fae0e24f7500d95a51e88eb9aed2e8
Ray Lee <rhlee@berkeley.edu>
2016-01-12 22:35:12 -0800

    PAHMA-1324: Clean up of sortable object number computation, and add support for No-Temp numbers.

--- src/main/webapp/tenants/pahma/js/pahma.js
+++ src/main/webapp/tenants/pahma/js/pahma.js
@@ -11,50 +11,39 @@ https://source.collectionspace.org/collection-space/LICENSE.txt
 var pahma = {};
 
 (function ($, fluid) {
-	var pad = function(s, len){
-		if (len-s.length+1 > 0) {
-			return (new Array((len-s.length+1) ).join("0")) + s;
+	var pad = function(s, len) {
+		if (len - s.length + 1 > 0) {
+			return (new Array((len - s.length + 1)).join("0")) + s;
 		}
 		else {
 			return s;
 		}
 	};
-	
+
 	var isNumber = function(n) {
 		return !isNaN(parseFloat(n)) && isFinite(n);
 	}
 
-        var trim = function(s) {
-	    var l=0; var r=s.length -1;
-	    while(l < s.length && s[l] == ' ')
-	      { l++; }
-	    while(r > l && s[r] == ' ')
-	      { r-=1; }
-	    return s.substring(l, r+1);
-	}
+	var objectNumberPattern = /^([cC](ons|ONS)?[\-\. ]?)?(No-Temp|[A-Z]+)?([\-\. ])?(\d+)([\-\. ])?(\d+)?([\.\- ]+)?(\d+)?([\.\- ]+)?(.*)$/;
+	//                          1    2                   3                4         5    6         7     8          9     10         11   
 
-	pahma.createSortableObjectNumber = function(objnum) {
-  		//            1    2                   3        4         5    6         7    8          9     10         11   
-  		var objRe = /^([cC](ons|ONS)?[\-\. ]?)?([A-Z]+)?([\-\. ])?(\d+)([\-\. ])?(\d+)?([\.\- ]+)?(\d+)?([\.\- ]+)?(.*)$/;
-  		var objTokens = objRe.exec(objnum);
-  		if (objTokens == null) {
-			return objnum;
-		  }
-		  else {
-			for ( i = 0 ; i < objTokens.length ; i = i+1 ) {
-				if (!objTokens[i]) {
-					objTokens[i] = '';
-				}
-      				else {
-        				if (isNumber(objTokens[i])) {
-					objTokens[i] = pad(objTokens[i],6);
-        			}
-			}
-			objTokens[i] = objTokens[i] + ' ';
+	pahma.computeSortableObjectNumber = function(objectNumber) {
+		var sortableObjectNumber = objectNumber;
+		var tokens = objectNumberPattern.exec(objectNumber);
+
+		if (tokens) {
+			var parts = [tokens[3], tokens[5], tokens[7], tokens[9], tokens[11]]
+				.filter(function(token) {
+					return token;
+				})
+				.map(function(token) {
+					return (isNumber(token) ? pad(token, 6) : token);
+				});
+
+			sortableObjectNumber = parts.join(' ').trim();
 		}
-		if (objTokens[3] == ' ') objTokens[3] == ''; // zap empty alphabetic prefix
-		return trim(objTokens[3]+objTokens[5]+objTokens[7]+objTokens[9]+objTokens[11]);
-		}
+	
+		return sortableObjectNumber;
 	}
 	
 	var removeTimestamp = function(datetime) {
